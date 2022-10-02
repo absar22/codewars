01c491120cc717da72c4197f3f0cd445376533c3
Absar Ahmad <absarahmad137@gmail.com>
2022-10-02 12:34:42 +0530

    Add coding challenge attempt for 10-2-22.

--- CC.2022-10-1/app.js
+++ CC.2022-10-1/app.js
@@ -4,3 +4,17 @@
 
 // Return true if the array contains the value, false if not.
 
+// P: Given array A and a random value X. want to see if A has X in it. A can have numbers, strings. X can be a numbers ora string. Is it ever have an empty array? Undefined ? Or any special character
+// R: True if X is in A, False if X is Not 
+// E: Given [1,2,3] as A and 4 as X , should return false
+//    Given [7,5,8] as A and 5 as X , should return true
+//    Given [] as A and 5 as X , should return true False
+// P: Make a function that take two parameter : an array and a random number
+function check(a, x) {
+  // Look through the a and see if there are any element that match x.
+  // conditional :IF they do return TRUE else FALSE
+  return a.map(element => element === x ? true : false)
+}
+
+console.log(check([1,2,3], 4)) //should return false
+console.log(check([7,5,8], 5)) //should return true
