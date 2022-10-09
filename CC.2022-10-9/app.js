2ac70e616c47d740c1e5cd7be70bb1d41a31f8f7
Absar Ahmad <absarahmad137@gmail.com>
2022-10-09 19:49:22 +0530

    Coding challenge for 10-09-22

--- CC.2022-10-9/app.js
+++ CC.2022-10-9/app.js
@@ -48,3 +48,20 @@ function smash(words) {
 }
 
 console.log(smash(["hello", "world", "this", "is", "great"]));
+
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+ let str = ''
+ for(let i = 0; i< n ; i++){
+  str += s
+ }
+ return str
+}
+
+console.log(repeatStr(6, 'Hello'))
