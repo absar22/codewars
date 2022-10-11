b9b4af6deddda3fba74dc7a4bab133fd7bce383c
Absar Ahmad <absarahmad137@gmail.com>
2022-10-11 22:03:44 +0530

    Another coding challenge

--- CC.2022-10-10/app.js
+++ CC.2022-10-10/app.js
@@ -81,3 +81,17 @@ function stringToArray(string){
   return string.split(' ')
  
  }
+
+
+//  I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+//  P.S. Each array includes only integer numbers. Output is a number too.
+ 
+ 
+function arrayPlusArray(arr1, arr2) {
+  let array1 = arr1.reduce((acc, c) => acc + c)
+  let array2 = arr2.reduce((acc,c) => acc + c)
+  return array1 + array2
+ }
+
+ console.log(arrayPlusArray([1,2,3], [1,2,3]), 12)
