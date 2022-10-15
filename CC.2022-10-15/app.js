a3220f551be3929668452030a2d537672194f9fc
Absar Ahmad <absarahmad137@gmail.com>
2022-10-15 10:24:22 +0530

    Another coding challenge

--- CC.2022-10-15/app.js
+++ CC.2022-10-15/app.js
@@ -64,3 +64,25 @@ function countBy(x, n) {
 }
 
 console.log(countBy(3,10))
+
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+function sumMix(x){
+  let sum = 0
+  for(let i = 0; i< x.length; i++){
+   if(typeof x[i] === 'number'){
+     sum += x[i]
+   }else if (typeof x[i] === 'string'){
+     x[i] = Number(x[i])
+     sum += x[i]
+   }
+  }
+   return sum
+ }
+ 
+ console.log(sumMix([1,'2',3]))
+ 
