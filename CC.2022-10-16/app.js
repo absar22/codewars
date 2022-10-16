e6789961df60e54a42d3501d8c96cb2b2ea9b967
Absar Ahmad <absarahmad137@gmail.com>
2022-10-16 18:37:52 +0530

    Another solution for the code

--- CC.2022-10-16/app.js
+++ CC.2022-10-16/app.js
@@ -121,3 +121,10 @@ function doubleInteger(i) {
   return i**2;
   
 }
+
+// Another solution 
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i**2;
+  
+}
