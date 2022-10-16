c89a791451b488f5149e1f07f59efd42c6a7695b
Absar Ahmad <absarahmad137@gmail.com>
2022-10-16 17:41:15 +0530

    Another easy way to solve this challenge

--- CC.2022-10-16/app.js
+++ CC.2022-10-16/app.js
@@ -76,3 +76,12 @@ class SmallestIntegerFinder {
   }
 }
 
+// Another easy way to solve this problem
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let sortedArray = args.sort((a,b)=> a-b)
+    return sortedArray[0]
+  } 
+}
+
