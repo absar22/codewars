6f97d5daed70bde7bf0f37c3611526da6a8cdbac
Absar Ahmad <absarahmad137@gmail.com>
2022-10-17 18:02:06 +0530

    Added solution for the coding challenge

--- CC.2022-10-17/app.js
+++ CC.2022-10-17/app.js
@@ -17,3 +17,11 @@
 // What We're Testing
 // We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
 // Advanced users may find this extremely easy and can easily write this in one line.
+
+function sum (numbers) {
+  "use strict";
+   return numbers.reduce((acc,c) => acc + c, 0)
+  
+};
+
+console.log(sum([1,2,3-9,7,0.8]))
