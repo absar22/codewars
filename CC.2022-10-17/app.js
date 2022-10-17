ebcba7e15adba4ce334a76addc90930799fa8c5a
Absar Ahmad <absarahmad137@gmail.com>
2022-10-17 18:56:47 +0530

    Coding challenge with solution

--- CC.2022-10-17/app.js
+++ CC.2022-10-17/app.js
@@ -25,3 +25,20 @@ function sum (numbers) {
 };
 
 console.log(sum([1,2,3-9,7,0.8]))
+
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let newArray = []
+  
+  for(let i = n; i > 0; i--){
+    newArray.push(i)
+  }
+  return newArray
+  
+//   return [];
+  
+};
