65adbe94f5c4f3b72ebe399b6cc0aca031d87812
Absar Ahmad <absarahmad137@gmail.com>
2022-10-19 20:59:04 +0530

    Adding solution to the question

--- CC.2022-10-19/app.js
+++ CC.2022-10-19/app.js
@@ -7,3 +7,22 @@
 // if bmi <= 30.0 return "Overweight"
 
 // if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  //   return "";
+    let value = weight / height **2
+    if(value <= 18.5){
+      return 'Underweight'
+    }else if (value <= 25.0){
+      return 'Normal'
+    }else if (value <= 30.0){
+      return 'Overweight'
+    }else if (value > 30){
+      return 'Obese'
+    }
+    return value
+    
+    
+  }
+  
+  console.log(bmi(12,34))
