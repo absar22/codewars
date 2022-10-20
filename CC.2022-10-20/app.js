751d2809dad3c2b4a80b5517cb777b4e8a198b72
Absar Ahmad <absarahmad137@gmail.com>
2022-10-20 21:49:11 +0530

    Solution

--- CC.2022-10-20/app.js
+++ CC.2022-10-20/app.js
@@ -11,3 +11,20 @@ function greet(name){
 // Write a function which calculates the average of the numbers in a given list.
 
 // Note: Empty arrays should return 0.
+
+
+function findAverage(array) {
+  // your code here
+//   return 0;
+  
+  
+  let sum = 0
+  for(let i = 0; i< array.length; i++){
+    sum += array[i]
+  }
+  if(array.length === 0){
+    return 0
+  } else {
+    return sum/array.length
+  }
+}
