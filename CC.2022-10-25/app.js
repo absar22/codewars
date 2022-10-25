de63aec7ff0c232ae41b1ee7858c0171e338de23
Absar Ahmad <absarahmad137@gmail.com>
2022-10-25 21:28:46 +0530

    Coding question

--- CC.2022-10-25/app.js
+++ CC.2022-10-25/app.js
@@ -16,3 +16,11 @@ function sum (numbers) {
   return numbers.reduce((acc,c) => acc + c, 0)
   
 };
+
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
