3d4236532fc357db3f18c8d59ecd68abf0942652
Absar Ahmad <absarahmad137@gmail.com>
2022-10-29 13:02:49 +0530

    Solution

--- CC.2022-10-28/app.js
+++ CC.2022-10-28/app.js
@@ -30,4 +30,19 @@ console.log(boolToWord(true))
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 
-
+function countPositivesSumNegatives(input) {
+  // your code here
+  let count = 0
+  let sum = 0
+  if(input === null || input.length === 0){
+    return []
+  }
+ for(let i = 0; i < input.length; i++){
+   if(input[i] > 0){
+     count++
+   }else{
+     sum += input[i]
+   }
+ }
+  return [count, sum]
+}
