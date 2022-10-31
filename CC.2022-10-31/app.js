9965cfc08e3ee54ee566386eed36c07950dffcbc
Absar Ahmad <absarahmad137@gmail.com>
2022-10-31 21:04:59 +0530

    Coding challenge

--- CC.2022-10-31/app.js
+++ CC.2022-10-31/app.js
@@ -21,3 +21,12 @@ function paperwork(n, m) {
  console.log(paperwork(5,-5), 0)
  console.log(paperwork(-5,-5), 0)
  console.log(paperwork(-5,5), 0)
+
+
+//  Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
