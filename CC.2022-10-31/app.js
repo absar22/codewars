90de83085649fef7563e393468f1034d429ba184
Absar Ahmad <absarahmad137@gmail.com>
2022-10-31 21:06:12 +0530

    Coding challenge solution

--- CC.2022-10-31/app.js
+++ CC.2022-10-31/app.js
@@ -30,3 +30,14 @@ function paperwork(n, m) {
 // case	return
 // name equals owner	'Hello boss'
 // otherwise	'Hello guest'
+
+
+function greet (name, owner) {
+  // Add code here
+  if(name === owner){
+    return 	'Hello boss'
+  }else {
+    return 	'Hello guest'
+  }
+  
+}
