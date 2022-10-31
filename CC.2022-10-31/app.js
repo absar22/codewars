55f4e9d3de2c86144ee325b6db7389ecc6b77998
Absar Ahmad <absarahmad137@gmail.com>
2022-10-31 09:32:20 +0530

    Coding solution for the challenge

--- CC.2022-10-31/app.js
+++ CC.2022-10-31/app.js
@@ -6,3 +6,18 @@
 // n= 5, m=5: 25
 // n=-5, m=5:  0
 
+function paperwork(n, m) {
+  if( n < 0 && m < 0){
+    return 0
+  }else if(n > 0 && m < 0){
+    return 0
+  }else if (n < 0 && m > 0){
+    return 0
+  }else {
+    return n * m
+  }
+ }
+ console.log(paperwork(5,5), 25)
+ console.log(paperwork(5,-5), 0)
+ console.log(paperwork(-5,-5), 0)
+ console.log(paperwork(-5,5), 0)
