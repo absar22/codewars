988f3fee17ba18cfa3a61854c6d37672d55c5e9c
Absar Ahmad <absarahmad137@gmail.com>
2022-11-07 21:23:32 +0530

    Coding challenge question

--- CC.2022-11-7/app.js
+++ CC.2022-11-7/app.js
@@ -46,3 +46,10 @@ function getAge(inputString){
 }
 
 console.log(getAge(`2 year old`))
+
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
