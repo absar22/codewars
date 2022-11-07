436c9ddedb7c4f2687c771c15b7ed39c4462ead0
Absar Ahmad <absarahmad137@gmail.com>
2022-11-07 09:26:22 +0530

    Coding solution

--- CC.2022-11-7/app.js
+++ CC.2022-11-7/app.js
@@ -9,3 +9,14 @@ function addFive(num) {
 }
 
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  let currentHealth = health - damage
+  if(currentHealth < 0){
+    return 0
+  }else {
+    return currentHealth
+  }
+    
+  }
+  console.log(combat(100,30))
