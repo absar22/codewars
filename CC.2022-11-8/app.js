63bea2ca31fb1dbfcae2c170af4330a0c7f46387
Absar Ahmad <absarahmad137@gmail.com>
2022-11-08 08:54:20 +0530

    Coding solution

--- CC.2022-11-8/app.js
+++ CC.2022-11-8/app.js
@@ -4,3 +4,12 @@
 
 // The array will never be empty.
 
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  let sum = 0
+  for(let i = 0; i < marks.length; i++){
+    sum += marks[i]
+  }
+  let average = sum/marks.length
+  return Math.floor(average)
+}
