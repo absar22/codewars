cf7a423b4e9b5229a0dbe2ed9cf62ba536cbaf98
Absar Ahmad <absarahmad137@gmail.com>
2022-11-08 19:36:03 +0530

    Solution

--- CC.2022-11-8/app.js
+++ CC.2022-11-8/app.js
@@ -43,3 +43,11 @@ console.log(isDivisible(12,3,4))
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, when the input is green, output should be yellow.
+
+function updateLight(current) {
+  
+  //your code here!
+if(current === 'green') return 'yellow'
+  else if (current === 'yellow') return 'red'
+  else if (current === 'red') return 'green'
+}
