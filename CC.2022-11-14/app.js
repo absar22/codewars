b420f515e00fe0bea92dcd16fd11e925fb63728d
Absar Ahmad <absarahmad137@gmail.com>
2022-11-14 21:27:04 +0530

    Coding solution

--- CC.2022-11-14/app.js
+++ CC.2022-11-14/app.js
@@ -1,2 +1,7 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+function reversePokemon(value){
+  return value.reverse()
+}
+console.log(reversePokemon(['pikachu', 'balbasaur', 'dragonite', 'togopie', 'charmender', 'chikorita']))
