51c76652d40b727c6609153eb8eedb3853cb653b
Absar Ahmad <absarahmad137@gmail.com>
2022-11-14 21:30:17 +0530

    Coding question

--- CC.2022-11-14/app.js
+++ CC.2022-11-14/app.js
@@ -17,3 +17,9 @@ console.log(compareSquareAndCube([2,2,2], [2,2,2]))
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+function isMultiple(arr){
+  return arr.filter((e,i) => e % i === 0)
+}
+
+console.log(isMultiple([22, -6, 32, 82, 9, 25]))
