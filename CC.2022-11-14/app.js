3e2c769f09d2548e32c49fac41954a08df6f2b4a
Absar Ahmad <absarahmad137@gmail.com>
2022-11-14 21:28:45 +0530

    Coding solution

--- CC.2022-11-14/app.js
+++ CC.2022-11-14/app.js
@@ -7,3 +7,8 @@ function reversePokemon(value){
 console.log(reversePokemon(['pikachu', 'balbasaur', 'dragonite', 'togopie', 'charmender', 'chikorita']))
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+function compareSquareAndCube(a,b){
+  return a.reduce((acc,c) => acc + c**2, 0) > b.reduce((acc,c) => acc + c**3, 0)
+}
+console.log(compareSquareAndCube([2,2,2], [2,2,2]))
