e67b6e3c0c923393617cfb3e618e5f02dab36864
Absar Ahmad <absarahmad137@gmail.com>
2022-11-16 22:01:52 +0530

    Coding answer

--- CC.2022-11-16/app.js
+++ CC.2022-11-16/app.js
@@ -5,3 +5,16 @@
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 // Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+  // Your solution here
+  let cost = 40 *  d
+  if(d >= 7){
+    return cost - 50
+  }else if (d >= 3) {
+    return cost - 20
+  } else {
+    return cost
+  }
+  
+}
