272f2b2705a8ea8cc3c439bf312c131fe1e1c7d5
Absar Ahmad <absarahmad137@gmail.com>
2022-11-22 22:04:30 +0530

    Coding solution

--- CC.2022-11-22/app.js
+++ CC.2022-11-22/app.js
@@ -30,3 +30,7 @@ function main (verb, noun)  {
 // Variable assignment
 // Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
 
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
