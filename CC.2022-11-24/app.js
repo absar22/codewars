c8d65ef9fd4f21d11bfe2824420457c6fbb2dcf2
Absar Ahmad <absarahmad137@gmail.com>
2022-11-24 13:14:03 +0530

    Coding solution

--- CC.2022-11-24/app.js
+++ CC.2022-11-24/app.js
@@ -26,3 +26,29 @@ function twoDecimalPlaces(n) {
 // subtraction = subt
 
 // Note: All math operations will be: a (operation) b
+
+
+
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return  a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return  a**b
+}
+  
+function subt(a,b){
+  return a - b
+}
