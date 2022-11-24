1179d311c35b08f546be876f049ccb8c5d905b36
Absar Ahmad <absarahmad137@gmail.com>
2022-11-24 13:13:19 +0530

    Coding question

--- CC.2022-11-24/app.js
+++ CC.2022-11-24/app.js
@@ -8,3 +8,21 @@ function twoDecimalPlaces(n) {
   // Your code here
   return Number(n.toFixed(2))
 }
+
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
