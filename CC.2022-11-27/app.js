49c656900616b4e26c845a62212b539017d5c89f
Absar Ahmad <absarahmad137@gmail.com>
2022-11-27 21:42:04 +0530

    Coding solution

--- CC.2022-11-27/app.js
+++ CC.2022-11-27/app.js
@@ -66,3 +66,18 @@ function ensureQuestion(s) {
 // 6 returns "Friday"
 // 7 returns "Saturday"
 // Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+function whatday(num) { 
+
+  // put your code here
+
+   if(num === 1) return 'Sunday'
+  else if(num === 2) return 'Monday'
+  else if(num === 3) return 'Tuesday'
+  else if(num === 4) return 'Wednesday'
+  else if(num === 5) return 'Thursday'
+  else if(num === 6) return 'Friday'
+  else if(num === 7) return 'Saturday'
+  else return 'Wrong, please enter a number between 1 and 7'
+
+}
