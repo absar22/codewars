b09a5ca5eb9b16c21304a030e0436c0c6501334c
Absar Ahmad <absarahmad137@gmail.com>
2022-11-28 21:39:26 +0530

    Coding solution

--- CC.2022-11-28/app.js
+++ CC.2022-11-28/app.js
@@ -24,3 +24,13 @@ function reverseWords(str){
 // 5  =>  4
 // 15  =>  13
 // -3  =>  -3
+
+function getRealFloor(n) {
+  if((n < 13) && (n > 0)){
+    return n -1
+  }else if (n > 13){
+    return n - 2
+  }else if (n < 0){
+    return n
+  }else return 0
+}
