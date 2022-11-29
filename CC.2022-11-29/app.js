99fc9bdc48089e0bf5e967654c021409c09c978d
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 21:44:41 +0530

    Coding solution

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -125,3 +125,7 @@ function well(x){
 //   Define a function that removes duplicates from an array of numbers and returns it as a result.
 
 // The order of the sequence has to stay the same.
+
+function distinct(a) {
+  return a.filter((e,i) => a.indexOf(e) === i)
+}
