dcf86ff8f8003ef645dffddfd6d0a1eeb6fede3f
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 21:55:02 +0530

    Coding question

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -143,3 +143,14 @@ function multipleOfIndex(array) {
   // good luck
   return array.filter((e,i) => e % i == 0)
 }
+
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
