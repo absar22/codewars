e652f6c40c0e08c9d77f64db96ceafc3a2e3789a
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 13:24:18 +0530

    Coding solution

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -53,3 +53,7 @@ function getPlanetName(id){
 
 
 // This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
