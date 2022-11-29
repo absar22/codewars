6a36d1d982f74a1fb5d737d095f693d3da5738a5
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 19:33:41 +0530

    Coding question

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -68,3 +68,14 @@ function args_count(){
   return arguments.length
 }
 console.log(args_count(1,2,3,4))
+
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
