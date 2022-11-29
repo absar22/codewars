6950ee0f8b1da471c0628a57402a63b2b50b94d9
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 21:03:19 +0530

    Coding question

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -103,4 +103,10 @@ function sumOfDifferences(arr) {
 
 // add the value "codewars" to the already defined websites array
 
-websites.push('codewars')
+// websites.push('codewars')
+
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
