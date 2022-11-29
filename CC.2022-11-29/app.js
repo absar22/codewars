ad100dc3382a403641708b7ee28a450e68418cd4
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 21:44:03 +0530

    Coding question

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -121,3 +121,7 @@ function well(x){
     if(result > 0) return 'Publish!'
     return 'Fail!'
   }
+
+//   Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
