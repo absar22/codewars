0ef88a93be94341e212d3c02d79de9c2c96c1878
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 18:20:24 +0530

    Coding solution

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -61,3 +61,10 @@ var name = a + b;
 // How many arguments
 // args_count(1, 2, 3) -> 3
 // args_count(1, 2, 3, 10) -> 4
+
+// Create a function called args_count that returns the number of arguments provided
+function args_count(){
+
+  return arguments.length
+}
+console.log(args_count(1,2,3,4))
