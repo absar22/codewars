5377b5b9bb93b3f6a68152269361ce6ea0c85371
Absar Ahmad <absarahmad137@gmail.com>
2022-11-29 21:04:04 +0530

    Coding solution

--- CC.2022-11-29/app.js
+++ CC.2022-11-29/app.js
@@ -110,3 +110,14 @@ function sumOfDifferences(arr) {
 
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
+function well(x){
+  let result = 0
+  for(let element of x){
+    if(element === 'good') result+= 1
+  }
+  
+  
+    if(result > 2) return 'I smell a series!'
+    if(result > 0) return 'Publish!'
+    return 'Fail!'
+  }
