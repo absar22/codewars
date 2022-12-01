0d6e9e9dc93667f5a38af4bb2381747f0f0326ff
Absar Ahmad <absarahmad137@gmail.com>
2022-12-01 08:54:36 +0530

    Coding solution

--- CC.2022-12-1/app.js
+++ CC.2022-12-1/app.js
@@ -5,3 +5,7 @@
 // Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
 
 // Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+function check(a,x){
+  return a.includes(x)
+};
