725c2dda4c8946b64a1393efd7b8a339496763e2
Absar Ahmad <absarahmad137@gmail.com>
2022-12-01 20:57:58 +0530

    Coding solution

--- CC.2022-12-1/app.js
+++ CC.2022-12-1/app.js
@@ -66,3 +66,12 @@ function _if(bool, func1, func2) {
   // ...
   return bool ? func1() : func2() 
 }
+
+// Carousel
+// Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+// When it's done, improve it so that the number of turns is given by the user.
+
+for(let i = 1; i <= 10; i++){
+  console.log(i)
+}
