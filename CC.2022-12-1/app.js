e1c8c0f7192fa46b241570cd1df1304837286937
Absar Ahmad <absarahmad137@gmail.com>
2022-12-01 22:04:47 +0530

    Coding question

--- CC.2022-12-1/app.js
+++ CC.2022-12-1/app.js
@@ -119,3 +119,15 @@ function remainder(n, m){
     return 0
   }
 }
+
+
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
