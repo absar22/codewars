bba226b972a2b0394a611d9e11cd3e5617562099
Absar Ahmad <absarahmad137@gmail.com>
2022-12-01 09:38:48 +0530

    Coding qustion

--- CC.2022-12-1/app.js
+++ CC.2022-12-1/app.js
@@ -9,3 +9,9 @@
 function check(a,x){
   return a.includes(x)
 };
+
+// I've hit a few bugs in my Java/Type/Coffee-script code recently, and I've traced the problem back to the global isNaN function I was using. I had expected it to be more discerning, but it's returning true for undefined right now.
+
+// Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+
+// Any solution is acceptable!
