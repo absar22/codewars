8ac855e24a0f32dd6fc6b670bcdcfc0b42bbe5b8
Absar Ahmad <absarahmad137@gmail.com>
2022-12-01 09:39:34 +0530

    Coding solution

--- CC.2022-12-1/app.js
+++ CC.2022-12-1/app.js
@@ -15,3 +15,9 @@ function check(a,x){
 // Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
 
 // Any solution is acceptable!
+
+const isReallyNaN = (val) => {
+  // return isNaN(val);  // wasn't working as planned :-(
+//   return true || false;
+ return Number.isNaN(val)
+};
