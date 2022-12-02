35fadd86e9e5024b24d63fdd7bb29d9780a44d15
Absar Ahmad <absarahmad137@gmail.com>
2022-12-02 07:32:14 +0530

    Coding solution

--- CC.2022-12-2/app.js
+++ CC.2022-12-2/app.js
@@ -34,3 +34,9 @@ function howManyLightsabersDoYouOwn(name) {
 // When you have finished the work, click "Run Tests" to see if your code is working properly.
 
 // In the end, click "Submit" to submit your code pass this kata.
+
+
+function helloWorld(){
+  let str = 'Hello World!'
+  console.log(str)
+}
