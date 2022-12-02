c80aad2e31f6c356938fdee94945f2356f3128c3
Absar Ahmad <absarahmad137@gmail.com>
2022-12-02 20:23:28 +0530

    Coding solution

--- CC.2022-12-2/app.js
+++ CC.2022-12-2/app.js
@@ -59,3 +59,15 @@ function hello(name) {
 }
 
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+
+function problem(x){
+  //your code here
+ if(Number(x)){
+   return x * 50 + 6
+ }else if (x === 0){
+   return x + 6
+ }else {
+   return 'Error'
+ }
+}
