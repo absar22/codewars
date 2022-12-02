b9dd929230f80cb32e082a10b58f012873fe8aad
Absar Ahmad <absarahmad137@gmail.com>
2022-12-02 20:22:28 +0530

    Coding question

--- CC.2022-12-2/app.js
+++ CC.2022-12-2/app.js
@@ -57,3 +57,5 @@ function hello(name) {
   else return 'Hello, World!'
 
 }
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
