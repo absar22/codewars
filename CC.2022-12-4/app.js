c05e739de131bc70bbcfc609d8b4823c903aeebc
Absar Ahmad <absarahmad137@gmail.com>
2022-12-04 22:03:40 +0530

    Coding question

--- CC.2022-12-4/app.js
+++ CC.2022-12-4/app.js
@@ -95,3 +95,12 @@ function replace(s){
 function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
+
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
