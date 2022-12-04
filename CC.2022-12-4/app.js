c375fc0d50f0428940d2f7a598102f1836d99883
Absar Ahmad <absarahmad137@gmail.com>
2022-12-04 20:25:12 +0530

    Coding solution

--- CC.2022-12-4/app.js
+++ CC.2022-12-4/app.js
@@ -6,3 +6,26 @@
 // replace("!Hi! Hi!") === "!H!! H!!"
 // replace("aeiou") === "!!!!!"
 // replace("ABCDE") === "!BCD!"
+
+
+
+// Answer 
+
+
+//PREP
+// Parameter => We take strings as a parameter , it can be uppercase or lowercase 
+// Return => we replace any vowel 'a e i o u' (both uppercase or lowercase) with '!' 
+
+// Example 
+// ("Hi!") === "H!!"
+// ("!Hi! Hi!") === "!H!! H!!"
+// ("aeiou") === "!!!!!"
+// ("ABCDE") === "!BCD!"
+function replace(s){
+  // We can use replace method here
+ return s.replace(/[aeiou]/gi, '!')  // we use g for => global search & i for => caseSensitvity
+    
+  
+}
+
+
