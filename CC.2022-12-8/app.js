219c6d9cf46af358e9dc2bd35af01ff6eb1b1f86
Absar Ahmad <absarahmad137@gmail.com>
2022-12-08 20:52:32 +0530

    coding question

--- CC.2022-12-8/app.js
+++ CC.2022-12-8/app.js
@@ -41,3 +41,7 @@ function popElement(arr){
   arr.pop()
   return arr
 }
+
+
+// While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.
+
