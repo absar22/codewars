82ea0eb1a2d90f78a191a99023dce00431ac59a3
Absar Ahmad <absarahmad137@gmail.com>
2022-12-08 20:02:59 +0530

    Coding question

--- CC.2022-12-8/app.js
+++ CC.2022-12-8/app.js
@@ -8,3 +8,12 @@ for(let i = 1; i <= 1000; i++){
 }
 
 console.log(websites)
+
+// Task
+// I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
