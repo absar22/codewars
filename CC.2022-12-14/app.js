103e7e21eb6170717d6b7aa6694084aa505b453e
Absar Ahmad <absarahmad137@gmail.com>
2022-12-14 20:14:05 +0530

    Coding question

--- CC.2022-12-14/app.js
+++ CC.2022-12-14/app.js
@@ -13,3 +13,9 @@
 function friend(friends){
   return friends.filter(x => x.length === 4)
 }
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
