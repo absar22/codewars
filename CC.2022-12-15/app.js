0d5804f00b2ae03d0d8278015b23996c11b8e48c
Absar Ahmad <absarahmad137@gmail.com>
2022-12-15 21:52:25 +0530

    Coding solution

--- CC.2022-12-15/app.js
+++ CC.2022-12-15/app.js
@@ -5,3 +5,26 @@
 // Your task is to write a function that takes a string and return a new string with all vowels removed.
 
 // For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// PREP
+
+// P => we have string as  a paramenter
+// R => We want to remove every vowel lettere form the string
+
+function disemvowel(str) {
+  // Declare a variable thar we will remove
+  let vowel = 'aeiouAEIOU'
+  // Place where we will put our result
+  let newString = ''
+
+  for(let char of str){
+    if(!vowel.includes(char)){
+      newString += char
+    }
+  }
+  return newString
+}
+
+
+// Example 
+console.log(disemvowel('This website is for losers LOL!'), 'Ths wbst s fr lsrs LL!')
