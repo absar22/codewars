b29d61efab951092198b4eacbfe99159bd5b4dbe
Absar Ahmad <absarahmad137@gmail.com>
2022-12-16 20:59:22 +0530

    Coding solution

--- CC.2022-12-16/app.js
+++ CC.2022-12-16/app.js
@@ -6,3 +6,22 @@
 
 
 
+// PREP
+
+// PARAMETER => We have an array as a parameter 
+// RETURN => We want to return the sum of first lowest and second lowest number
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  // we sorted the number in assending order
+  let sorted = numbers.sort((a,b) => a - b)
+  // we will return the first and second sum
+  return sorted[0] + sorted[1]
+  
+  
+}
+
+// EXAMPLE 
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]),13)
+console.log(sumTwoSmallestNumbers(10, 343445353, 3453445, 3453545353453),3453455)
