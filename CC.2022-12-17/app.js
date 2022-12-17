4b5b808188e631b6e53553f4bd75072d03f55212
Absar Ahmad <absarahmad137@gmail.com>
2022-12-17 21:48:06 +0530

    Coding solution

--- CC.2022-12-17/app.js
+++ CC.2022-12-17/app.js
@@ -157,3 +157,10 @@ var findAverage = function (nums) {
 // Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
 // Note: String will have at least one element; words will always be separated by a space.
+
+
+function addLength(str) {
+  //start-here
+      let array = str.split(' ')
+    return array.map(x=> `${x} ${x.length}`)
+  }
