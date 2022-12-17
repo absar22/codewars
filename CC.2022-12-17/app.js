87c407d71ac8874fca6db1c083c1641ec4b0a8f5
Absar Ahmad <absarahmad137@gmail.com>
2022-12-17 18:36:20 +0530

    Coding solution

--- CC.2022-12-17/app.js
+++ CC.2022-12-17/app.js
@@ -18,3 +18,12 @@ function fixTheMeerkat(arr) {
 
 // array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 // array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+
+function index(array, n){
+  //your code here
+if(n >= array.length){
+  return -1
+}
+  return Math.pow(array[n], n)
+}
