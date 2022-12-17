5df8b662a91f9dc99a9117fd833300c57d348d6a
Absar Ahmad <absarahmad137@gmail.com>
2022-12-17 21:47:32 +0530

    Coding question

--- CC.2022-12-17/app.js
+++ CC.2022-12-17/app.js
@@ -145,3 +145,15 @@ items.push({a: "b", c: "d"})
 var findAverage = function (nums) {
   return nums.reduce((acc,c) => acc + c, 0)/nums.length
 }
+
+
+
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+// Example(Input --> Output)
+
+// "apple ban" --> ["apple 5", "ban 3"]
+// "you will win" -->["you 3", "will 4", "win 3"]
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+// Note: String will have at least one element; words will always be separated by a space.
