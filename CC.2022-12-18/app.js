5931770dac1f894324e36dc363d4dae4898a884b
Absar Ahmad <absarahmad137@gmail.com>
2022-12-18 22:04:19 +0530

    Coding solution

--- CC.2022-12-18/app.js
+++ CC.2022-12-18/app.js
@@ -69,3 +69,10 @@ function strCount(str, letter){
 // Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
 
 // Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+
+function capitalizeWord(word) {
+  let value =   word[0].toUpperCase() + word.slice(1)
+   return value;
+   
+ }
