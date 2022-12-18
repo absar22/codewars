dd8060b04b1ba013a074a4a06ba73c8d94f970af
Absar Ahmad <absarahmad137@gmail.com>
2022-12-18 19:25:35 +0530

    Coding solution

--- CC.2022-12-18/app.js
+++ CC.2022-12-18/app.js
@@ -3,3 +3,11 @@
 // Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 
 // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+
+
+function feast(beast, dish) {
+  //your function here
+  if(beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1])return true
+    else return false
+  }
