30fa6011ee690b529dbacaf520010f954060bf61
Absar Ahmad <absarahmad137@gmail.com>
2022-12-18 21:14:41 +0530

    Coding solution

--- CC.2022-12-18/app.js
+++ CC.2022-12-18/app.js
@@ -51,3 +51,15 @@ function feast(beast, dish) {
 // ("Hello", "o")  ==>  1
 // ("Hello", "l")  ==>  2
 // ("", "z")       ==>  0
+
+
+function strCount(str, letter){  
+  //code here
+  let count = 0
+  for(let i = 0;i < str.length; i++){
+    if(str[i] === letter){
+      count ++
+    }
+  }
+  return count 
+}
