0c21b8f14a1a390fda75598e5f3d807fcaf0b748
Absar Ahmad <absarahmad137@gmail.com>
2022-12-18 19:49:31 +0530

    Coding question

--- CC.2022-12-18/app.js
+++ CC.2022-12-18/app.js
@@ -11,3 +11,16 @@ function feast(beast, dish) {
   if(beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1])return true
     else return false
   }
+
+
+
+  
+  // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+  // Hint for R users:
+  
+  // The length of string is not always the same as the number of characters
+  // For example: (Input1, Input2) --> output
+  
+  // ("1", "22") --> "1221"
+  // ("22", "1") --> "1221"
