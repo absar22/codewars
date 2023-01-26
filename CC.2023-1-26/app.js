462d63bb41166656d9ad9b142ab34d925d3166ed
Absar Ahmad <absarahmad137@gmail.com>
2023-01-26 10:51:28 +0530

    Coding solution

--- CC.2023-1-26/app.js
+++ CC.2023-1-26/app.js
@@ -13,3 +13,20 @@
 // 'B' and 'g' returns 0
 
 // '0' and '?' returns -1
+
+
+
+
+function sameCase(a, b){
+  if(a.toLowerCase() === a.toUpperCase() || b.toLowerCase() === b.toUpperCase()){
+    return -1
+  }
+  else if ((a === a.toUpperCase() && b === b.toUpperCase()) ||
+           (a === a.toLowerCase() && b === b.toLowerCase())){
+    return 1
+  }
+   
+else {
+  return 0
+}
+  }
