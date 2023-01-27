03d715d02794489de05fe445dc4f1f6f4b4aab6e
Absar Ahmad <absarahmad137@gmail.com>
2023-01-27 12:03:03 +0530

    coding solution

--- CC.2023-1-27/app.js
+++ CC.2023-1-27/app.js
@@ -16,3 +16,13 @@
 // #Output
 
 // The middle character(s) of the word represented as a string.
+
+
+function getMiddle(s){
+  let middleIndex = Math.floor(s.length/2);
+  if(s.length % 2 === 0){
+    return s[middleIndex - 1] + s[middleIndex]
+  }else {
+    return s[middleIndex]
+  }
+  }
