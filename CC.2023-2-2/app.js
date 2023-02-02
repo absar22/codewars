d854537d4a15b739ac3eded3f3153c2a97fcceee
Absar Ahmad <absarahmad137@gmail.com>
2023-02-02 20:17:13 +0530

    Coding question

--- CC.2023-2-2/app.js
+++ CC.2023-2-2/app.js
@@ -9,3 +9,13 @@ function oddCount(n){
  if(n % 2 === 0) return n / 2
   else return Math.floor(n/2)
 }
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
