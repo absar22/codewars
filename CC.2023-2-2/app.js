0b8664e8e7dfc9da3f69630f2283cc3a3e4a5c24
Absar Ahmad <absarahmad137@gmail.com>
2023-02-02 20:08:52 +0530

    coding solution

--- CC.2023-2-2/app.js
+++ CC.2023-2-2/app.js
@@ -3,3 +3,9 @@
 // Examples (Input -> Output)
 // 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+function oddCount(n){
+  // your code here
+ if(n % 2 === 0) return n / 2
+  else return Math.floor(n/2)
+}
