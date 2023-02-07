c14e56dc4af11bb92d429751fc4a9aab877fc6c2
Absar Ahmad <absarahmad137@gmail.com>
2023-02-07 16:47:24 +0530

    function Hero (name) {

--- CC.2023-2-7/app.js
+++ CC.2023-2-7/app.js
@@ -30,3 +30,12 @@ function orderOperations () {
 // health	100
 // damage	5
 // experience	0
+
+function Hero (name) {
+  // add default values here
+  this.name = name || 'Hero';
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
