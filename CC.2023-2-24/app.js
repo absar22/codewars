7900e1f6842cb66c1f759a86838138322f49f963
Absar Ahmad <absarahmad137@gmail.com>
2023-02-24 14:36:53 +0530

    coding question

--- CC.2023-2-24/app.js
+++ CC.2023-2-24/app.js
@@ -13,3 +13,13 @@ function checkAlive (health) {
     return true
   }
 }
+
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
