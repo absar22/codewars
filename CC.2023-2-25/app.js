12705783b012b211b6c325648f128738a300dc37
Absar Ahmad <absarahmad137@gmail.com>
2023-02-25 10:04:36 +0530

    Coding solution

--- CC.2023-2-25/app.js
+++ CC.2023-2-25/app.js
@@ -29,3 +29,11 @@ function squareOrSquareRoot(array) {
 
 // generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
 // generateRange(1, 10, 3) // should return array of [1,4,7,10]
+
+function generateRange(min, max, step){
+  let result = []
+  for(let i = min ; i <=max ; i+=step){
+    result.push(i)
+  }
+  return result
+}
