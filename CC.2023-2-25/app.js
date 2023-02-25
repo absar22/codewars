6d3352b163eeb56430b20bae84ae91782d8f7f4d
Absar Ahmad <absarahmad137@gmail.com>
2023-02-25 09:43:57 +0530

    Coding solution

--- CC.2023-2-25/app.js
+++ CC.2023-2-25/app.js
@@ -8,3 +8,15 @@
 // [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 // Notes
 // The input array will always contain only positive numbers, and will never be empty or null.
+
+
+function squareOrSquareRoot(array) {
+  return array.map(x => {
+    let sqrt = Math.sqrt(x)
+    if(sqrt % 1 === 0){
+      return sqrt
+    }else {
+      return x ** 2
+    }
+  })  
+}
