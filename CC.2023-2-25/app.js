770ffc259772479eae9453d8fc4fba32f932c1dc
Absar Ahmad <absarahmad137@gmail.com>
2023-02-25 11:47:15 +0530

    coding qustion

--- CC.2023-2-25/app.js
+++ CC.2023-2-25/app.js
@@ -37,3 +37,11 @@ function generateRange(min, max, step){
   }
   return result
 }
+
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
