de8f437e087791b4088fc658658e83da76846e53
Absar Ahmad <absarahmad137@gmail.com>
2023-02-26 13:27:26 +0530

    Coding question

--- CC.2023-2-26/app.js
+++ CC.2023-2-26/app.js
@@ -40,3 +40,12 @@ var isSquare = function(n){
   return Number.isInteger(result)
   
 }
+
+
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
