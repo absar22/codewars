51d1a97be0d6b9419a23ea1516d6adde1eb7bc3b
Absar Ahmad <absarahmad137@gmail.com>
2023-02-26 13:28:01 +0530

    Coding solution

--- CC.2023-2-26/app.js
+++ CC.2023-2-26/app.js
@@ -49,3 +49,8 @@ var isSquare = function(n){
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(x => typeof x === 'number' && x === 0  || typeof  x !== 'string' && Number(x) ) 
+}
