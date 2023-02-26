4b7efd480d7329ea3e6d5104450e487d6b173ddf
Absar Ahmad <absarahmad137@gmail.com>
2023-02-26 13:30:21 +0530

    Coding solution

--- CC.2023-2-26/app.js
+++ CC.2023-2-26/app.js
@@ -54,3 +54,9 @@ function filter_list(l) {
   // Return a new array with the strings filtered out
   return l.filter(x => typeof x === 'number' && x === 0  || typeof  x !== 'string' && Number(x) ) 
 }
+
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(x => typeof x === 'number')
+}
