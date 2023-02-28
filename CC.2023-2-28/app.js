fc3678c3f7fc8a5be42cca2fbe7daf778809ed70
Absar Ahmad <absarahmad137@gmail.com>
2023-02-28 19:39:26 +0530

    Coding question

--- CC.2023-2-28/app.js
+++ CC.2023-2-28/app.js
@@ -5,3 +5,12 @@
 function findShort(s){
   return s.split(' ').map(x => x.length).sort((a,b) => a - b  ,0)[0]
 }
+
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
