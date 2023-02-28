78e60a29185ad099fb27198a0fe4a8e816f93b30
Absar Ahmad <absarahmad137@gmail.com>
2023-02-28 19:54:11 +0530

    Coding question

--- CC.2023-2-28/app.js
+++ CC.2023-2-28/app.js
@@ -19,3 +19,11 @@ function accum(s) {
 	// your code
   return s.split('').map((x,i) => x.toUpperCase() + x.toLowerCase().repeat(i)).join('-')
 }
+
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
