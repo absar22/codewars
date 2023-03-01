a14b516da6b11ef6edce7606e2021e3b5868ce09
Absar Ahmad <absarahmad137@gmail.com>
2023-03-01 09:14:41 +0530

    Coding solution

--- CC.2023-3-1/app.js
+++ CC.2023-3-1/app.js
@@ -6,3 +6,7 @@
 
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function(){
+  return this.split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+}
