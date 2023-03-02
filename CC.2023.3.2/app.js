ddf7bc3ddea0a0036b1e6511ac97749ef51dd165
Absar Ahmad <absarahmad137@gmail.com>
2023-03-02 13:33:48 +0530

    Coding solution

--- CC.2023.3.2/app.js
+++ CC.2023.3.2/app.js
@@ -10,3 +10,25 @@
 // Method calling example:
 
 // fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+// Return an array
+function fizzbuzz(n)
+{
+  //
+ let result = []
+ for(let i = 1; i <= n; i++){
+   if(i % 3 === 0 && i % 5 === 0) {
+     result.push('FizzBuzz')
+   }
+   else if (i % 5 === 0) {
+     result.push('Buzz')
+   }
+   else if ( i % 3 === 0) {
+     result.push('Fizz')
+   }
+   else {
+     result.push(i)
+   }
+ }
+  return result
+}
