e03a8492d5520042dd844adfc2b6161f05288884
Absar Ahmad <absarahmad137@gmail.com>
2023-03-03 19:44:08 +0530

    Coding question

--- CC.2023.3.3/app.js
+++ CC.2023.3.3/app.js
@@ -7,3 +7,9 @@ var capitals = function (word) {
 	// Write your code here
   return word.split('').map((x,i) => x === x.toUpperCase() ? i : undefined).filter(x => x !== undefined)
 };
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
