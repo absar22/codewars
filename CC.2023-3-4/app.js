0c0628e01314e64ef5f80f3d9dc0dacebdd16fed
Absar Ahmad <absarahmad137@gmail.com>
2023-03-04 20:27:57 +0530

    Coding solution

--- CC.2023-3-4/app.js
+++ CC.2023-3-4/app.js
@@ -11,3 +11,14 @@
 // Example
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data){
+ 
+  return data.map(x => {
+    if(x[0] >= 55 && x[1] > 7){
+      return 'Senior'
+    }else{
+      return 'Open'
+    }
+  })
+}
