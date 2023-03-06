6c38bac3c2be252ea469bf196e9733c29e52f022
Absar Ahmad <absarahmad137@gmail.com>
2023-03-06 19:44:25 +0530

    Coding solution

--- CC.2023.3-6/app.js
+++ CC.2023.3-6/app.js
@@ -3,3 +3,11 @@
 // Task
 // Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
 
+class Cat extends Animal {
+  constructor(name){
+    super (name)
+  }
+  speak(){
+    return `${this.name} meows.`
+  }
+  }
