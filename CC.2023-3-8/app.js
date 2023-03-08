beb3759ed3e1789dc120265df1b0b6088a69a826
Absar Ahmad <absarahmad137@gmail.com>
2023-03-08 10:18:06 +0530

    coding solution

--- CC.2023-3-8/app.js
+++ CC.2023-3-8/app.js
@@ -4,3 +4,16 @@
 // type({}) == 'object'
 // type('') == 'string'
 // type(NaN) == 'number'
+
+function type(value) {
+  // P a variable of type
+  // R string representing type of variable
+  // E 
+  // P  
+    if(Array.isArray(value)) return 'array'
+    const detectedType = typeof value
+    if(detectedType !== 'object') return detectedType
+    if(value === null) return 'null'
+    return Object.getPrototypeOf(value).constructor.name.toLowerCase()
+  }
+  
