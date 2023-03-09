78793c2eb1b012da7a672be4ea43a04a01ab67ba
Absar Ahmad <absarahmad137@gmail.com>
2023-03-09 09:56:55 +0530

    codung solution

--- CC.2023-3-9/app.js
+++ CC.2023-3-9/app.js
@@ -7,3 +7,20 @@
 // Output : "HEllO WOrld!"
 
 // Note: Y is not a vowel in this kata.
+
+function swap (string) {
+  // P given a string both in uppercase and lowercase
+  // R capitalize only vowels string
+  // E 
+  // P
+     let vowels = 'aeiouAEIOU'
+     let result = ''
+     for (let i = 0; i < string.length; i++){
+       if(vowels.includes(string[i])){
+         result += string[i].toUpperCase()
+       }else {
+         result += string[i]
+       }
+     }
+     return result
+  }
