bf78a620c5a274a6d241a53b45f2c36e9b434ce6
Absar Ahmad <absarahmad137@gmail.com>
2023-03-25 15:20:46 +0530

    Coding solution

--- CC.2023-3-25/app.js
+++ CC.2023-3-25/app.js
@@ -7,3 +7,12 @@
 
 // celsius = (fahrenheit - 32) * (5/9)
 // Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+function weatherInfo (temp) {
+  let c =  (temp - 32) * (5/9)
+  if (c <= 0)
+    return (c + " is freezing temperature")
+  else
+    return (c + " is above freezing temperature")
+}
+
