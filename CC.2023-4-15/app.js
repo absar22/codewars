f8d3ae057cee848443b8e71c6fc9d21d2423edec
Absar Ahmad <absarahmad137@gmail.com>
2023-04-15 09:41:26 +0530

    coding solution

--- CC.2023-4-15/app.js
+++ CC.2023-4-15/app.js
@@ -19,3 +19,38 @@ function isPowerOfTwo(n){
  
   return (n &  (n-1)) === 0
 }
+
+
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+// Example:
+// calculator(1,2,"+"); //=> result will be 3
+// calculator(1,2,"&"); //=> result will be "unknown value"
+// calculator(1,"k","*"); //=> result will be "unknown value"
+// Good luck!
+
+function calculator(a, b, sign) {
+  // Attempt to convert input strings to numbers
+  a = parseFloat(a);
+  b = parseFloat(b);
+
+  if (sign === '+') {
+    return a + b;
+  } else if (sign === '-') {
+    return a - b;
+  } else if (sign === '*') {
+    return a * b;
+  } else if (sign === '/') {
+    return a / b;
+  } else if (isNaN(a) || isNaN(b)) {
+    return "unknown value";
+  } else {
+    return "unknown value";
+  }
+}
