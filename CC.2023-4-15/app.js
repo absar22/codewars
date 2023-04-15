3a7676c0fc6266ac823a7b5df8c2f2c8e31e6d35
Absar Ahmad <absarahmad137@gmail.com>
2023-04-15 15:11:17 +0530

    coding solution

--- CC.2023-4-15/app.js
+++ CC.2023-4-15/app.js
@@ -90,3 +90,7 @@ function getSize(l, w, h){
 
 // This function should return an object, but it's not doing what's intended. What's wrong?
 
+function mystery() {
+  var results =  {sanity: 'Hello'};
+  return results;
+}
