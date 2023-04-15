f6a7b204b323d4fbbc8e6d71cbf872742a12ba2a
Absar Ahmad <absarahmad137@gmail.com>
2023-04-15 14:42:19 +0530

    coding solution

--- CC.2023-4-15/app.js
+++ CC.2023-4-15/app.js
@@ -42,11 +42,7 @@ function calculator(a, b, sign) {
 
 
 
-//   Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
 
-// If the tail is right return true, else return false.
-
-// The arguments will always be non empty strings, and normal letters.
 
   if (sign === '+') {
     return a + b;
@@ -60,5 +56,24 @@ function calculator(a, b, sign) {
     return "unknown value";
   } else {
     return "unknown value";
+  }
+}
+
+
+//   Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+// If the tail is right return true, else return false.
+
+// The arguments will always be non empty strings, and normal letters.
+
+
+  
+function correctTail(bod, tail) {
+  var sub = bod.substr(bod.length - tail.length);
+  
+  if (sub === tail) {
+    return true;
+  } else {
+    return false;
   }
 }
