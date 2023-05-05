5826c9231489acd656afd281fbb565bd2ebf2e1b
Absar Ahmad <absarahmad137@gmail.com>
2023-05-05 10:43:19 +0530

    Coding solution

--- CC.2023-5-5/app.js
+++ CC.2023-5-5/app.js
@@ -6,3 +6,12 @@
 // There will never be a case when both are empty as nothing is going on!!
 
 // Note: The character is a string of length 1 or an empty string.
+
+function contamination(text, char){
+  // Code here ;)
+  let result = []
+  for(let i = 0; i < text.length; i++){
+    result.push(char)
+  }
+  return result.join('')
+}
