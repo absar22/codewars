84741ad7b9f4ba4eb8cf7ddc0706f015652dd42d
Absar Ahmad <absarahmad137@gmail.com>
2023-05-05 11:37:40 +0530

    coding quetion

--- CC.2023-5-5/app.js
+++ CC.2023-5-5/app.js
@@ -15,3 +15,15 @@ function contamination(text, char){
   }
   return result.join('')
 }
+
+
+
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
