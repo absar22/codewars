d3b9684610536eb2e75aad0b30caf7df4135d180
Absar Ahmad <absarahmad137@gmail.com>
2023-05-10 11:40:48 +0530

    coding solution

--- CC.2023-5-10/app.js
+++ CC.2023-5-10/app.js
@@ -3,3 +3,10 @@
 // In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
 
 // For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+
+function integrate(coefficient, exponent) {
+  const val1 = coefficient / (exponent + 1 )
+  const val2 = exponent + 1
+  return `${val1}x^${val2}`
+}
+
