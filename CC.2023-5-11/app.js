46fd8540468f2f29b63c16edf8867c985be81b5f
Absar Ahmad <absarahmad137@gmail.com>
2023-05-11 12:52:25 +0530

    coding solution

--- CC.2023-5-11/app.js
+++ CC.2023-5-11/app.js
@@ -5,3 +5,10 @@
 // Now, basically, when a person hears about Freud, s/he hears "sex" because for Freud, everything was related to, and explained by sex.
 
 // In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string.
+
+
+function toFreud(string) {
+  if(string.length === 0) return ''
+return string.split(' ').map(() => 'sex').join(' ')
+
+}
