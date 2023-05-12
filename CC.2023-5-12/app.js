182ef39f55554eea7dbf1a0ce4522a8f5ba17954
Absar Ahmad <absarahmad137@gmail.com>
2023-05-12 15:16:26 +0530

    coding solution

--- CC.2023-5-12/app.js
+++ CC.2023-5-12/app.js
@@ -28,3 +28,29 @@ function anyArrows(arrows){
 // According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
 
 // You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
+
+class Human {
+  constructor() {}
+}
+
+class Man extends Human {
+  constructor() {
+    super();
+  }
+}
+
+class Woman extends Human {
+  constructor() {
+    super();
+  }
+}
+
+class God {
+  static create() {
+    const adam = new Man();
+    const eve = new Woman();
+    return [adam, eve];
+  }
+}
+
+
