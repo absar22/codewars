e590a7b316bf1e2377ba0ff9b8f6ef567d415c9f
Absar Ahmad <absarahmad137@gmail.com>
2023-05-14 11:04:01 +0530

    coding question

--- CC.2023-5-14/app.js
+++ CC.2023-5-14/app.js
@@ -14,3 +14,7 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
     return twiceAge - dadYearsOld
   }
 }
+
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
