53f28da23c0d8a6768a9988240f8599d26576973
Absar Ahmad <absarahmad137@gmail.com>
2023-05-17 09:16:43 +0530

    coding solution

--- CC.2023-5-17/app.js
+++ CC.2023-5-17/app.js
@@ -8,3 +8,11 @@
 
 // [] --> []
 // ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number = function(array) {
+  let result = [];
+  for(let i = 0; i < array.length; i++){
+    result.push(`${i + 1}: ${array[i]}`);
+  }
+  return result;
+};
