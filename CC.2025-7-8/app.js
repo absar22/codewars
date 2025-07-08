903983a1247bf5a7dd05ef19aaee51f2a78e436b
Absar Ahmad <absarahmad137@gmail.com>
2025-07-08 21:30:05 +0530

    codewars problem

---
+++ CC.2025-7-8/app.js
@@ -0,0 +1,28 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let sorted = numbers.sort((a, b) => a-b)
+  let sum = sorted[0] + sorted[1];
+  return sum;
+}
+
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+const rowSumOddNumbers = n => n ** 3
