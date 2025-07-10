b0eb8b0aaeccdada6481fe5626b15dab942b85b6
Absar Ahmad <absarahmad137@gmail.com>
2025-07-10 17:17:10 +0530

    8kyus problem

--- CC.2025-7-10/app.js
+++ CC.2025-7-10/app.js
@@ -14,3 +14,33 @@ class Ball{
     this.ballType = ballType
   }
 }
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+function remove (string) {
+  //coding and coding....
+  return string.endsWith('!') ? string.slice(0,-1) : string
+}
+
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+//   3 -->    15  (  3 * 5¹)
+//  10 -->   250  ( 10 * 5²)
+// 200 --> 25000  (200 * 5³)
+//   0 -->     0  (  0 * 5¹)
+//  -3 -->   -15  ( -3 * 5¹)
+// Fundamentals
+
+function multiply(number){
+  let val = Math.abs(number).toString().split('').length
+  return number * (5**val)
+}
