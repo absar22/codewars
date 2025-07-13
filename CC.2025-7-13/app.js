7e1026267fd30ca474af5180a2809e5b5bcdc0d5
Absar Ahmad <absarahmad137@gmail.com>
2025-07-13 10:09:02 +0530

    Another 7kyus

--- CC.2025-7-13/app.js
+++ CC.2025-7-13/app.js
@@ -15,3 +15,19 @@ function disemvowel(str) {
 // one liner solution
 
 const disemvowel = str => str.split('').filter(e =>  !'aeiouAEIOU'.includes(e)).join('')
+
+
+// You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Examples:
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
+
+function getMiddle(s) {
+  //Code goes here!
+    return s.length % 2 === 0 ?  s[Math.floor(s.length/2) -1] + s[Math.floor(s.length/2)] :  s[Math.floor(s.length/2)]
+}
