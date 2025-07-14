ac39dc820d88b67019556005cd3e7d0cdd3d2ed2
Absar Ahmad <absarahmad137@gmail.com>
2025-07-14 11:29:21 +0530

    anotherone

--- CC.2025-7-14/app.js
+++ CC.2025-7-14/app.js
@@ -46,3 +46,12 @@ function filter_list(l) {
   // Return a new array with the strings filtered out
   return l.filter(e => typeof e === 'number')
 }
+
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// function findShort(s){
+//   return s.split(' ').map(e => e.length).sort((a,b)=>a-b)[0]
+// }
