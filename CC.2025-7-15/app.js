10ba7703dfaad9e7365fc7afbe80688606e6e21f
Absar Ahmad <absarahmad137@gmail.com>
2025-07-15 12:42:19 +0530

    solutions

--- CC.2025-7-15/app.js
+++ CC.2025-7-15/app.js
@@ -49,3 +49,20 @@ function minMax(arr){
   let sorted = arr.sort((a,b) => a - b)
   return [sorted[0], sorted[sorted.length-1]]
 }
+
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+
+function sortByLength (array) {
+
+    return array.sort((a,b) => a.length - b.length)
+}
