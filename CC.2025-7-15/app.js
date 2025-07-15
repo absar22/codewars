ab1856241d7ff34ece1698f44e8eee7de6fef660
Absar Ahmad <absarahmad137@gmail.com>
2025-07-15 12:01:01 +0530

    solutions

--- CC.2025-7-15/app.js
+++ CC.2025-7-15/app.js
@@ -34,3 +34,18 @@ const binaryArrayToNumber = arr => {
   let val = arr.join('')
   return parseInt(val,2)
 };
+
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+
+function minMax(arr){
+  let sorted = arr.sort((a,b) => a - b)
+  return [sorted[0], sorted[sorted.length-1]]
+}
