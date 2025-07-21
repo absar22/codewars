de570da56771a8f768b28bb31e0887a2840cd1fc
Absar Ahmad <absarahmad137@gmail.com>
2025-07-21 21:51:58 +0530

    another 7kys solution

--- CC.2025-7-21/app.js
+++ CC.2025-7-21/app.js
@@ -178,3 +178,10 @@ function checkExam(array1, array2) {
   }
   return result
 }
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+
+const removeUrlAnchor = url =>  url.split('#')[0]
