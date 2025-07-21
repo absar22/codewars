923602c79e639a02b71bc85037f44f297bbc3e02
Absar Ahmad <absarahmad137@gmail.com>
2025-07-21 22:08:46 +0530

    another 7kys

--- CC.2025-7-21/app.js
+++ CC.2025-7-21/app.js
@@ -185,3 +185,10 @@ function checkExam(array1, array2) {
 // "www.codewars.com#about" --> "www.codewars.com"
 
 const removeUrlAnchor = url =>  url.split('#')[0]
+
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+const digits = n => n.toString().length;
