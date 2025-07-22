6d19e2dce81fffc09ca94e8e3424b8c5a7d98fc3
Absar Ahmad <absarahmad137@gmail.com>
2025-07-22 13:11:21 +0530

    more 7kys

--- CC.2025-7-22/app.js
+++ CC.2025-7-22/app.js
@@ -75,3 +75,18 @@ function accum(s) {
 const maskify = cc => cc.length <= 4 ? cc : '#'.repeat(cc.length - 4) + cc.slice(-4)
 
 
+
+
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+function bump(x){
+  let count = 0
+  for(let i = 0; i < x.length;i++){
+    if(x[i] === 'n'){
+      count += 1
+    }
+  }
+  return count <= 15 ?  'Woohoo!' : 'Car Dead'
+}
