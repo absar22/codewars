1d5508ac69a0c28dc187586609246067f36b2a26
Absar Ahmad <absarahmad137@gmail.com>
2025-07-24 21:11:11 +0530

    7kyscode

--- CC.2025-7-24/app.js
+++ CC.2025-7-24/app.js
@@ -12,3 +12,13 @@
 
 
 const STRANGE_STRING = "ß"
+
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// "312" should return "333122"
+// "102269" should return "12222666666999999999"
+
+function explode(s) {
+  return s.split('').map(x => x.repeat(+x)).join('')
+}
