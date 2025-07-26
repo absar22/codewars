3efebf0774e9f96be8fdd5d899685c85170fdf6e
Absar Ahmad <absarahmad137@gmail.com>
2025-07-27 00:38:47 +0530

    8kys codewars

--- CC.2025-7-27/app.js
+++ CC.2025-7-27/app.js
@@ -23,3 +23,14 @@ function howManydays(month) {
       return 31
   }
 }
+
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+
+const getASCII = c => c.charCodeAt(0)
+ 
+
+
