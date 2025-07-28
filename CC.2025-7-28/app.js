458e29e90f44f514bc0e721ddd054297f2011157
Absar Ahmad <absarahmad137@gmail.com>
2025-07-28 21:40:23 +0530

    7kyus codewars

--- CC.2025-7-28/app.js
+++ CC.2025-7-28/app.js
@@ -28,3 +28,21 @@ function vowelOne(s){
 let vowels = 'aeiouAEIOU'
 return s.split('').map(c => vowels.includes(c) ? '1' : '0').join('')
 }
+
+
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+
+
+function last(x){
+   return x.split(' ').sort((a,b)=>{
+     const lastA = a[a.length -1]
+     const lastB = b[b.length -1]
+     return lastA.localeCompare(lastB)
+   })
+}
