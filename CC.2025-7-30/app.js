0dc76384046118d56d9cae160ea4c36798368899
Absar Ahmad <absarahmad137@gmail.com>
2025-07-30 23:02:39 +0530

    7kyus code

--- CC.2025-7-30/app.js
+++ CC.2025-7-30/app.js
@@ -13,3 +13,19 @@ function createPhoneNumber(numbers){
   const thirdPart = numbers.slice(6,10).join('')
   return `(${firstPart}) ${secondPart}-${thirdPart}`
 }
+
+
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  if (enteredCode !== correctCode) return false
+  const current = new Date(currentDate)
+  const expired = new Date(expirationDate)
+  return current <= expired        
+}
