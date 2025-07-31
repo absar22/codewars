3c1377949afefd84ccdea6134802325ff8ac4481
Absar Ahmad <absarahmad137@gmail.com>
2025-07-31 21:56:42 +0530

    7kys codewars

--- CC.2025-7-30/app.js
+++ CC.2025-7-30/app.js
@@ -98,3 +98,36 @@ function rowWeights(array){
  }
   return [team1,team2]
 }
+
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// The order does not have to be strict: a sorted array can contain consecutive duplicates, e.g. [1, 1, 2, 3] is sorted in ascending order.
+
+// It is guaranteed that there will always be a unique valid answer. More precisely:
+
+// there will be no arrays with less than 2 elements
+// there will be no arrays where all elements are equal
+
+
+function isSortedAndHow(array) {
+   let assending = true
+   let dessinding = true
+   for(let i = 0 ; i < array.length;i++){
+     if(array[i] < array[i - 1]){
+       assending = false
+     }if(array[i] > array[i - 1]){
+       dessinding = false
+     }
+   }
+  if(assending){
+    return "yes, ascending"
+  }else if(dessinding){
+    return "yes, descending"
+  }else{
+    return "no"
+  }
+}
