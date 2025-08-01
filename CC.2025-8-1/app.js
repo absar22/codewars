2be018855c11eb86d50db744c8135764ebd5d00d
Absar Ahmad <absarahmad137@gmail.com>
2025-08-01 20:07:06 +0530

    8kys

--- CC.2025-8-1/app.js
+++ CC.2025-8-1/app.js
@@ -32,3 +32,19 @@ function switcheroo(x){
 function countDevelopers(list) {
  return list.filter(x => x.language === 'JavaScript' && x.continent === 'Europe').length
 }
+
+
+
+// Task
+// Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
