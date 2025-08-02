999c49224e9670295e439fbb03b1be365993c039
Absar Ahmad <absarahmad137@gmail.com>
2025-08-02 09:02:42 +0530

    7kys

--- CC.2025-8-2/app.js
+++ CC.2025-8-2/app.js
@@ -38,3 +38,26 @@ function getSum(a, b)
 // Note: keep the original order of the names in the output.
 
 const friend = f => f.filter(x => x.length === 4)
+
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+function XO(str) {
+str = str.toLowerCase()
+  let countX = 0
+  let countY = 0
+  for(let char of str){
+    if(char === 'x') countX++
+    if(char === 'o') countY++
+  }
+  return countX ===countY
+}
