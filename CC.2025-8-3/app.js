1c772fc99494725e91b9bfa3d0b9b8dff0f56ec7
Absar Ahmad <absarahmad137@gmail.com>
2025-08-03 09:07:41 +0530

    8kys

--- CC.2025-8-3/app.js
+++ CC.2025-8-3/app.js
@@ -29,3 +29,22 @@ function NameMe(first, last) {
     this.lastName = last;
     this.name = first + " " + last
 }
+
+
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+// snoopy.bark(); // return "Woof"
+// scoobydoo.bark(); // undefined
+// Use method prototypes to enable all Dogs to bark.
+
+function Dog (breed) {
+  this.breed = breed;
+  
+}
+
+Dog.prototype.bark = function(){
+  return 'Woof'
+}
+
+var snoopy = new Dog("Beagle");
+var scoobydoo = new Dog("Great Dane");
