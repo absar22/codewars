70000aed127572e30a58d601ae0f1cd388c2bfb4
Absar Ahmad <absarahmad137@gmail.com>
2025-08-03 10:10:59 +0530

    8kys

--- CC.2025-8-3/app.js
+++ CC.2025-8-3/app.js
@@ -99,3 +99,21 @@ class Cube {
     this.side = Math.abs(n);
   }
 }
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
