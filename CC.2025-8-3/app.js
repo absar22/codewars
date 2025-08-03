33adf319fa111fbe803792d743a745852129d981
Absar Ahmad <absarahmad137@gmail.com>
2025-08-03 09:42:22 +0530

    8kys

--- CC.2025-8-3/app.js
+++ CC.2025-8-3/app.js
@@ -48,3 +48,31 @@ Dog.prototype.bark = function(){
 
 var snoopy = new Dog("Beagle");
 var scoobydoo = new Dog("Great Dane");
+
+
+// Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+// If the user tries to guess more than the limit, the function should throw an error.
+// If the user guess is right it should return true.
+// If the user guess is wrong it should return false and lose a life.
+// Can you finish the game so all the rules are met?
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if(this.lives <= 0){
+      throw new Error('no guesses left')
+    }
+    
+    if(n === this.number){
+      return true
+    }else{
+      this.lives--
+      return  false
+    }
+  }
+}
