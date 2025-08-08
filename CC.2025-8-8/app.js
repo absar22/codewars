33492146dc4a90a3725f1af98a2c17cb0495670b
Absar Ahmad <absarahmad137@gmail.com>
2025-08-08 18:39:52 +0530

    7kys code

--- CC.2025-8-8/app.js
+++ CC.2025-8-8/app.js
@@ -99,3 +99,20 @@ function min(arr, toReturn) {
   let smallest = arr.slice().sort((a,b) => a - b)[0]
   return toReturn === 'value' ? smallest : arr.indexOf(smallest);
 }
+
+
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+
+function maxDiff(list) {
+  if(list.length===0) return 0
+  let max = Math.max(...list)
+  let min = Math.min(...list)
+  return max - min
+};
