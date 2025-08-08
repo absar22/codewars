f09416e936b61a0544d200786036d4e1f8abfcd5
Absar Ahmad <absarahmad137@gmail.com>
2025-08-08 17:44:37 +0530

    7kys code

--- CC.2025-8-8/app.js
+++ CC.2025-8-8/app.js
@@ -84,3 +84,18 @@ function minSum(arr) {
 function evenNumbers(array, number) {
   return array.filter(x => x % 2 === 0).slice(-number)
 }
+
+
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+// Some examples:
+
+// ([1,2,3,4,5], "value") should return 1
+// ([1,2,3,4,5], "index") should return 0
+
+function min(arr, toReturn) {
+  let smallest = arr.slice().sort((a,b) => a - b)[0]
+  return toReturn === 'value' ? smallest : arr.indexOf(smallest);
+}
