7b87b50a544777e9e15a5d1a02aca52461dc2bad
Absar Ahmad <absarahmad137@gmail.com>
2025-08-11 10:14:08 +0530

    7kys codewars

--- CC.2025-8-11/app.js
+++ CC.2025-8-11/app.js
@@ -52,3 +52,13 @@ function greetDevelopers(list) {
   })
   return list
 }
+
+
+
+// Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+
+// Examples
+// ["jo", "nelson", "jurie"] -->  ["Jo", "Nelson", "Jurie"]
+// ["KARLY", "DANIEL", "KELSEY"] --> ["Karly", "Daniel", "Kelsey"]
+
+const capMe = names => names.map(e => e[0].toUpperCase() + e.slice(1).toLowerCase())
