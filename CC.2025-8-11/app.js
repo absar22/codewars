abe615dab6b85f299167a1ed5c847f60f5f02fce
Absar Ahmad <absarahmad137@gmail.com>
2025-08-11 09:39:55 +0530

    7kys codewars

---
+++ CC.2025-8-11/app.js
@@ -0,0 +1,36 @@
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits in total1 and total2:
+
+//   (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+//                        6
+
+function calc(x){
+  let newArr = []
+  for(let i = 0; i < x.length;i++){
+    newArr.push(x.charCodeAt(i)) 
+  }
+  let total1 =  newArr.join('')
+  let total2 = total1.replaceAll('7','1')
+  
+  let sum1 = 0
+  for(let i = 0; i < total1.length ; i++){
+     sum1 += parseInt(total1[i]);
+  }
+  
+  let sum2 = 0
+  for(let i = 0; i < total2.length;i++){
+    sum2 += parseInt(total2[i])
+  }
+  
+  return sum1 - sum2
+}
