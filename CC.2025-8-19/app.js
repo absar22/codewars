4a505bf755c1003ea70f14f14611e8201c2577e8
Absar Ahmad <absarahmad137@gmail.com>
2025-08-19 21:42:09 +0530

    7kys codewars

--- CC.2025-8-19/app.js
+++ CC.2025-8-19/app.js
@@ -11,3 +11,25 @@
 function sumDigits(number) {
   return Math.abs(number).toString().split('').map(Number).reduce((acc,cv) => acc + Math.abs(cv),0)
 }
+
+
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+  
+return str.split('').reverse().filter(c => c.toLowerCase() !== c.toUpperCase()).join('');  
+  
+  
+}
