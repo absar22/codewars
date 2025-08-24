a4d34e59ec33ccaf47d6c8a2a1e2ac643433dd93
Absar Ahmad <absarahmad137@gmail.com>
2025-08-24 22:49:02 +0530

    CODEWAWRS 7KYS PROBLEM

--- CC.2025-8-24/app.js
+++ CC.2025-8-24/app.js
@@ -34,3 +34,49 @@ const solution = () => {
   })
 };
 
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+// Examples ( Input --> Output )
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1  #  because 114 is not a perfect square
+
+function findNextSquare(sq) {
+ let root = Math.sqrt(sq)
+ if(root % 1 === 0){
+   let perfectSum = root + 1
+   return perfectSum * perfectSum
+ }
+  return -1
+}
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000, but fixed tests go higher.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+// Note you should only return a number, the count of divisors. 
+// The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      count++;              
+      if (i !== n / i) {    
+        count++;
+      }
+    }
+  }
+  
+  return count;
+}
