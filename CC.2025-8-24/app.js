26f40089623f90082a1be8c7ce431dafb3028adc
Absar Ahmad <absarahmad137@gmail.com>
2025-08-24 23:09:31 +0530

    CODEWAWRS 7KYS PROBLEM

--- CC.2025-8-24/app.js
+++ CC.2025-8-24/app.js
@@ -80,3 +80,16 @@ function getDivisorsCnt(n) {
   
   return count;
 }
+
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+
+// Examples
+// [1, 3, 1] ==> 13
+// [5, 7, 5, 9, 7] ==> 579
+// [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+
+function minValue(values){
+ let val = values.filter((element,index,arr) => arr.indexOf(element) === index).sort((a,b) => a - b).join('')
+  return +val
+}
