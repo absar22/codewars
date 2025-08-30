7ea8e6ea3e5acf04bbd05083e4261ca0a0c02300
Absar Ahmad <absarahmad137@gmail.com>
2025-08-30 10:48:20 +0530

    8kys code

--- CC.2025-8-30/app.js
+++ CC.2025-8-30/app.js
@@ -97,3 +97,11 @@ function all( arr, fun ){
    }
   return true
 }
+
+// You are trying to put a hash in ruby or an object in javascript or java into an array, 
+// but it always returns error, solve it and keep it as simple as possible!
+
+items = []
+let obj =  {a: "b", c: "d"}
+
+items.push(obj)
