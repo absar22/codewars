6bb643ffb3586478202dce3c9589f335b90c0377
Absar Ahmad <absarahmad137@gmail.com>
2025-09-03 23:26:14 +0530

    added codewars 7kys

--- CC.2025-9-3/app.js
+++ CC.2025-9-3/app.js
@@ -19,3 +19,22 @@ function solve(arr){
     return count
   })
 };
+
+
+// Write a program to determine if a string contains only unique characters.
+//  Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive,
+//  e.g. 'a' and 'A' are considered different characters.
+
+function hasUniqueChars(str){
+  let arr = []
+  for(let i = 0; i < str.length;i++){
+    let char = str[i]
+    if(arr.includes(char)){
+      return false
+    }
+    arr.push(char)
+  }
+  return true
+}
