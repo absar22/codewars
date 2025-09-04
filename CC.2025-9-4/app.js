782b0d3261e22b5e4aaa32d9d6a38bc328401813
Absar Ahmad <absarahmad137@gmail.com>
2025-09-04 20:14:09 +0530

    codewars 7kys

--- CC.2025-9-4/app.js
+++ CC.2025-9-4/app.js
@@ -14,3 +14,28 @@ function catMouse(x){
  let distance = mousePos - catPos - 1
  return distance > 3 ? 'Escaped!' : 'Caught!'
 }
+
+// The vowel substrings in the word codewarriors are o,e,a,io. 
+// The longest of these has a length of 2. 
+// Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces,
+//  return the length of the longest vowel substring. Vowels are any of aeiou.
+
+// Good luck!
+
+// If you like substring Katas, please try:
+
+function solve(s){
+ let vowels = 'aeiou'
+ let currentVal = 0
+ let maxVal = 0
+ for(let char of s.toLowerCase()){
+   if(vowels.includes(char)){
+     currentVal++
+     if(currentVal > maxVal) maxVal = currentVal
+   }else{
+     currentVal = 0
+   }
+ }
+  return maxVal
+ 
+} 
