a29b0ab347404a5dd79314ddd3e1a8647f62c501
Absar Ahmad <absarahmad137@gmail.com>
2025-09-05 21:25:39 +0530

    codewars 8kys

--- CC.2025=9-5/app.js
+++ CC.2025=9-5/app.js
@@ -9,3 +9,11 @@
 function spEng(sentence){
   return sentence.toLowerCase().includes("english");
 }
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return results;
+}
