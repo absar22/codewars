ab1c2fd4f6c4736ab8e5d1e3b70a46616f0ab534
Absar Ahmad <absarahmad137@gmail.com>
2025-09-05 21:45:11 +0530

    codewars 8kys

--- CC.2025=9-5/app.js
+++ CC.2025=9-5/app.js
@@ -35,3 +35,43 @@ const isDivisible = (n, x, y) => n % x === 0 && n % y === 0
 function greet(name){
  return name === 'Johnny' ?  "Hello, my love!" : "Hello, " + name + "!"
 }
+
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break
+    case 2:
+      name = 'Venus'
+      break
+    case 3:
+      name = 'Earth'
+      break
+    case 4:
+      name = 'Mars'
+      break
+    case 5:
+      name = 'Jupiter'
+      break
+    case 6:
+      name = 'Saturn'
+      break
+    case 7:
+      name = 'Uranus'
+      break
+    case 8:
+      name = 'Neptune'
+      break
+      default:
+      name = 'Unknown'
+  }
+  
+  return name;
+}
