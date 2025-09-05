451c92fdc0b873be4035d8f2c731b05151043b82
Absar Ahmad <absarahmad137@gmail.com>
2025-09-05 21:39:00 +0530

    codewars 8kys

--- CC.2025=9-5/app.js
+++ CC.2025=9-5/app.js
@@ -27,3 +27,11 @@ function mystery() {
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
 const isDivisible = (n, x, y) => n % x === 0 && n % y === 0
+
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+function greet(name){
+ return name === 'Johnny' ?  "Hello, my love!" : "Hello, " + name + "!"
+}
