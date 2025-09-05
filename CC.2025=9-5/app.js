9a2cffcef080ff13307abd0ea9df3a5a41e061eb
Absar Ahmad <absarahmad137@gmail.com>
2025-09-05 21:58:42 +0530

    codewars 8kys

--- CC.2025=9-5/app.js
+++ CC.2025=9-5/app.js
@@ -86,3 +86,15 @@ function getPlanetName(id){
 function oddCount(n){
   return Math.floor(n / 2)
 }
+
+
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number) {
+    const newArray = [];
+    for (let counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+    return newArray;
+}
