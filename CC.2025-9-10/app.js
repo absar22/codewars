283dc04b9a7d01ecb99d28a03df4b9e447452099
Absar Ahmad <absarahmad137@gmail.com>
2025-09-10 13:40:05 +0530

    codewars 6kys

--- CC.2025-9-10/app.js
+++ CC.2025-9-10/app.js
@@ -20,3 +20,32 @@ function duplicateEncode(word){
        .join('')
 
 }
+
+
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+function alphabetPosition(text) {
+  text = text.toLowerCase()
+  let val = []
+  for(let i = 0; i < text.length; i++){
+    let char = text[i]
+    if(char >= 'a' && char <= 'z'){
+         val.push(char.charCodeAt(0) - 96)
+    }
+    
+ 
+  }
+  return val.join(' ')
+  
+}
