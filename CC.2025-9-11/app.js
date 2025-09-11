feb2225f41a8275bbcd78bce8798e3da3aac9f40
Absar Ahmad <absarahmad137@gmail.com>
2025-09-11 13:12:43 +0530

    codewars 8kys

--- CC.2025-9-11/app.js
+++ CC.2025-9-11/app.js
@@ -52,3 +52,19 @@ function pillars(numPill, dist, width) {
   let distance = dist * 100;
   return (numPill - 1) * distance + (numPill > 2 ? (numPill - 2) * width : 0);
 }
+
+
+// Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+
+// Examples
+// (1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// (-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+// (1, 15, 20) -> [1]
+
+function generateRange(min, max, step){
+ let arr = []
+ for(let i = min; i <= max; i+=step){
+   arr.push(i)
+ }
+  return arr
+}
