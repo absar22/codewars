90f100c82258a0fcde04e3eed4580b99b1af7ed2
Absar Ahmad <absarahmad137@gmail.com>
2025-09-11 19:06:59 +0530

    codewars 8kys

--- CC.2025-9-11/app.js
+++ CC.2025-9-11/app.js
@@ -138,3 +138,15 @@ function convertToCelsius (temperature) {
   var celsius = (temperature - 32) * (5/9)
   return celsius
 }
+
+
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
