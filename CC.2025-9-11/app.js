cc5ab616236c5623d3898de2c217a2007e831d6b
Absar Ahmad <absarahmad137@gmail.com>
2025-09-11 15:27:29 +0530

    codewars 8kys

--- CC.2025-9-11/app.js
+++ CC.2025-9-11/app.js
@@ -100,3 +100,7 @@ function sameCase(a, b) {
   }
 return 0
 }
+
+
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+const include = (arr, item) =>  arr.includes(item)
