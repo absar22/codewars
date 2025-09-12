30b3218508fbcc45b3fc966a7c5515d67f7fb941
Absar Ahmad <absarahmad137@gmail.com>
2025-09-12 23:37:08 +0530

    codewars 8kys

--- CC.2025-9-12/app.js
+++ CC.2025-9-12/app.js
@@ -82,3 +82,30 @@ function quadrant(x, y) {
 // Output will be the shortened string, e.g., "Friday May 2".
 
 const shortenToDate = longDate => longDate.split(',')[0]
+
+
+// GET /
+// This route will read the message parameter and send a JSON response containing the echoed message value. The response should have a 200 status.
+
+// An example query string is ?message=hello which would be echoed as {"message": "hello"}. An empty string, ?message=, is valid and should be echoed normally.
+
+// Respond with a 422 status and the JSON {"error": "'message' was not provided"} when no query parameter message exists.
+
+// Respond with a 422 status and the JSON {"error": "'message' was not a string"} when the message was provided, but of a type other than a string.
+
+// POST /
+// This route will parse the request body as JSON and echo a response containing the value from the "message" key of the request. The JSON response is in the same format as the GET above: {"message": "some message"}. Set a 200 status on the response to indicate that the request was OK. An empty string ("message": "") is valid and should be echoed.
+
+// Respond with a 422 status when no "message" key exists, or if the message type is other than a string. The JSON errors for these cases are identical to those used in GET.
+
+// For either method, a request is still valid if additional parameters or keys are present in the query or JSON body. You can ignore them and echo message as normal.
+
+// Notes
+// Please use the variable name app to implement your server. Don't use app.listen() to start the Express server; the test suite will invoke the route handlers directly on the app object.
+
+// Your solution file is automatically included with the test suite, so no export is necessary.
+
+// Test input strings consist of alphanumeric characters and spaces.
+
+// If you're not familiar with the Express concepts needed to complete this kata, feel free to reference the documentation and other resources as you build your solution.
+
