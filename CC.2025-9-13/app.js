6bcdc28720b570a97f42af700d2c4f15379ec2ca
Absar Ahmad <absarahmad137@gmail.com>
2025-09-13 12:21:48 +0530

    codewars 8kyu

--- CC.2025-9-13/app.js
+++ CC.2025-9-13/app.js
@@ -45,3 +45,29 @@ String.prototype.digit = function(char) {
 // "Hi"      ---> "Hi!"
 
 const remove  = string =>  `${string.replaceAll('!', '')}!`
+
+// Write a simple function to check if the string contains the word hallo in different languages.
+
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+// Notes
+
+// you can assume the input is a string.
+// to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+// function should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+  
+ let arr = ['hello','ciao','salut','hallo','hola','ahoj','czesc']
+ for(let i = 0; i < arr.length;i++){
+   if(greetings.toLowerCase().includes(arr[i])) return true
+ }
+  return false
+}
