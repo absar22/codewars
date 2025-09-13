d9e10558a82324d42b58ce40f232074cb7cfd585
Absar Ahmad <absarahmad137@gmail.com>
2025-09-13 12:44:58 +0530

    codewars 8kyu

--- CC.2025-9-13/app.js
+++ CC.2025-9-13/app.js
@@ -71,3 +71,12 @@ function validateHello(greetings) {
  }
   return false
 }
+
+// You'll be given a string, and have to return the sum of all characters as an int.
+//  The function should be able to handle all printable ASCII characters.
+
+// Examples:
+
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+const uniTotal = string  =>  string.split('').map(x => x.charCodeAt(0)).reduce((a,b) => a + b, 0)
