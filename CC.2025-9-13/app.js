3744d32f619dda1941eb9e145dce5ac41f10b36b
Absar Ahmad <absarahmad137@gmail.com>
2025-09-13 13:14:37 +0530

    codewars 8kyu

--- CC.2025-9-13/app.js
+++ CC.2025-9-13/app.js
@@ -80,3 +80,19 @@ function validateHello(greetings) {
 // uniTotal("a") == 97
 // uniTotal("aaa") == 291
 const uniTotal = string  =>  string.split('').map(x => x.charCodeAt(0)).reduce((a,b) => a + b, 0)
+
+
+
+// It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+
+// If the winner is George Saint Pierre he will obviously say:
+
+// "I am not impressed by your performance."
+// If the winner is Conor McGregor he will most undoubtedly say:
+
+// "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+function quote(fighter) {
+ if(fighter.toLowerCase() === 'george saint pierre') return "I am not impressed by your performance."
+  else if (fighter.toLowerCase() === 'conor mcgregor') return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
