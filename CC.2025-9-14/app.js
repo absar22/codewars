3c81ae980ce1f59aefcec3874cdabc7d0922c315
Absar Ahmad <absarahmad137@gmail.com>
2025-09-14 23:36:37 +0530

    codewars 5kys

--- CC.2025-9-14/app.js
+++ CC.2025-9-14/app.js
@@ -26,3 +26,34 @@ function generateHashtag(str) {
   return value.length > 140 ? false : value;
 }
 
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+
+function towerBuilder(nFloors) {
+  let arr = []
+  for(let i = 0; i < nFloors; i++){
+    let stars = '*'.repeat(2 * i + 1)
+    let spaces = ' '.repeat(nFloors - i - 1)
+    arr.push(spaces+stars+spaces)
+  }
+  return arr
+}
