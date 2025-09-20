67cde5fd13e991c39def72376a67561f93e0d45d
Absar Ahmad <absarahmad137@gmail.com>
2025-09-20 20:08:48 +0530

    codewars 8kys

---
+++ CC.2025-9-20/app.js
@@ -0,0 +1,19 @@
+// Task
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+// Examples
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+function flickSwitch(arr){
+  let boolean = true
+  return arr.map(e => {
+    if(e === 'flick') {
+      boolean = !boolean
+    }
+    return boolean
+  })
+}
