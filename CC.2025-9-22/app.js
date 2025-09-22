d1aab32359f1097b87570ff6c2ae5b821898dd87
Absar Ahmad <absarahmad137@gmail.com>
2025-09-22 22:51:59 +0530

    codewars 8kys

--- CC.2025-9-22/app.js
+++ CC.2025-9-22/app.js
@@ -36,4 +36,25 @@ return string.split(' ').map(() => 'sex').join(' ')
 
 function describeAge(age) {
  return `You're a(n) ${age<=12?'kid':age<=17?'teenager':age<=64?'adult':'elderly'}`
+
 }
+
+
+// Your task
+// Create a package.json configuration and assign it to the configuration constant so it can be tested.
+
+// To pass the kata you need only the least that a real package file would require.
+
+// Notes
+// the kata version validation doesn't handle all cases and isn't 100% accurate, so don't try to be too fancy, stick to with the simple
+// to simplify the license validation this kata only allows the following
+// ISC
+// MIT
+// UNLICENSED
+// SEE LICENSE IN filename
+const configuration = {
+  "name": "your-pack-name",
+  "version": "1.0.0"
+};
+
+module.exports = configuration
