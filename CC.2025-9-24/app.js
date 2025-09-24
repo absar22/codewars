81cd09e7b2507e7813df9012fa9275c31bd6162c
Absar Ahmad <absarahmad137@gmail.com>
2025-09-24 11:21:54 +0530

    codewars 6kys

--- CC.2025-9-24/app.js
+++ CC.2025-9-24/app.js
@@ -29,3 +29,25 @@ var runLengthEncoding = function(str){
   }
   return result
 }
+
+
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+// The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+function validParentheses(parenStr) {
+  let val = 0
+ for (char of parenStr){
+   if(char === '(' ){
+      val++
+   }else{
+      val--
+   }
+   if(val < 0) return false
+ }
+  return val === 0
+}
