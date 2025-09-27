e609009695bb7fd0eed497b58730f3f9145ad726
Absar Ahmad <absarahmad137@gmail.com>
2025-09-27 22:28:56 +0530

    codewars 7kys

--- CC.2025-9-27/app.js
+++ CC.2025-9-27/app.js
@@ -2,3 +2,15 @@
 Math.roundTo = function (number, precision) {
   return +number.toFixed(precision)
 }
+
+// Task
+// Given a number, determine if it is tidy or not.
+function tidyNumber(n){
+   n =  n.toString().split('').map(Number)
+  for(let i = 0; i < n.length; i ++){
+    if(n[i] > n[i+1]){
+      return  false
+    }
+  }
+  return true
+}
