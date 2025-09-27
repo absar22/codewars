16b4fc2d422da4b51008c9b9de73bdfce389eba4
Absar Ahmad <absarahmad137@gmail.com>
2025-09-27 22:45:00 +0530

    codewars 7kys

--- CC.2025-9-27/app.js
+++ CC.2025-9-27/app.js
@@ -14,3 +14,25 @@ function tidyNumber(n){
   }
   return true
 }
+
+// Task:
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+// Note:Returning the pattern is not the same as Printing the pattern.
+// Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+// There are no whitespaces in the pattern.
+// Pattern:
+// 1
+// 22
+// 333
+// ....
+// .....
+// nnnnnn
+function pattern(n){
+ var output="";
+  for(let i = 1; i <= n ; i++){
+    output += String(i).repeat(i)+(i < n ? '\n' : "")
+  }
+ return output;
+}
