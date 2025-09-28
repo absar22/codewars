6ea67a62d683d2231521eb688e88cada58b01209
Absar Ahmad <absarahmad137@gmail.com>
2025-09-28 21:30:52 +0530

    codewars 6kys

--- CC.2025-9-28/app.js
+++ CC.2025-9-28/app.js
@@ -147,3 +147,24 @@ function removeConsecutiveDuplicates(string) {
   }
   return result.join(' ')
 }
+
+// Complete the solution so that it splits the string into pairs of two characters.
+//  If the string contains an odd number of characters then it should replace the missing second character of the final pair with an 
+// underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+   let arr = []
+   for(let i = 0; i < str.length; i+=2){
+     let vals = str.slice(i,i+2)
+     if(vals.length === 1){
+       vals+= '_'
+     } 
+     arr.push(vals)
+   }
+  return arr
+}
