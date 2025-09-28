2623b9c53d213dafa5ee3c5797df9a4b7f67faa2
Absar Ahmad <absarahmad137@gmail.com>
2025-09-28 16:16:30 +0530

    codewars 7kys

--- CC.2025-9-28/app.js
+++ CC.2025-9-28/app.js
@@ -128,3 +128,22 @@ function decipherThis(str) {
     return firstLetter + restStr
   }).join(' ')
 }
+
+
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+// Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
+
+function removeConsecutiveDuplicates(string) {
+ let arr =  string.split(' ')   
+  let result = []
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 0 || arr[i] !== arr[i + 1]){
+      result.push(arr[i])
+    }
+  }
+  return result.join(' ')
+}
