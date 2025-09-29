814ad9e487f6546ccdc1d891dc852e5d011dac55
Absar Ahmad <absarahmad137@gmail.com>
2025-09-29 12:35:20 +0530

    codewars 7kys

--- CC.2025-9-29/app.js
+++ CC.2025-9-29/app.js
@@ -88,3 +88,27 @@ return x.map(nums => {
   else if(nums == 29) return ' '
 }).join('')
 }
+
+
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// your function should return true.
+
+function isSameLanguage(list) {
+  const lang = list[0].language
+  for(let i = 0; i < list.length; i++){
+    if(list[i].language !== lang){
+      return false
+    }
+  }
+  return true
+}
