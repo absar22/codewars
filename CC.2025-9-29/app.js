3618d0f4170beb77d2aa8fe3d7a2570aac657ec9
Absar Ahmad <absarahmad137@gmail.com>
2025-09-29 10:23:35 +0530

    codewars 7kys

--- CC.2025-9-29/app.js
+++ CC.2025-9-29/app.js
@@ -54,3 +54,22 @@ function stray(n) {
  n.sort((a,b) => a - b)
   return n[0] === n[1] ? n[n.length - 1] : n[0]
 }
+
+
+// Your task is to return one of the following strings:
+
+// < firstName here >, < country here > of the first Python developer who has signed up; or
+// There will be no Python developers if no Python developer has signed up.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+// your function should return Victoria, Puerto Rico
+
+function getFirstPython(list) {
+ let pythonDev = list.find(lang => lang.language === 'Python')
+ return pythonDev ? `${pythonDev.firstName}, ${pythonDev.country}` : 'There will be no Python developers'
+}
