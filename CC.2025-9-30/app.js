634f95a0cf99035e2ad2e986aaf580f16d975117
Absar Ahmad <absarahmad137@gmail.com>
2025-09-30 22:04:33 +0530

    codewars 7kys

--- CC.2025-9-30/app.js
+++ CC.2025-9-30/app.js
@@ -20,3 +20,18 @@ function removeRotten(bagOfFruits) {
 function isItLetter(char) {
   return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
 }
+
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+
+// Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+function calculate(str) {
+  //your code here...
+  let val  = str
+    .replaceAll('plus', '+')
+    .replaceAll('minus', '-')
+ return eval(val).toString()
+}
