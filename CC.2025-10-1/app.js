d473a781361bd3232f7a52ca00548403f070b1cf
Absar Ahmad <absarahmad137@gmail.com>
2025-10-01 14:39:26 +0530

    codewars 7kys

--- CC.2025-10-1/app.js
+++ CC.2025-10-1/app.js
@@ -79,3 +79,25 @@ function covfefe(str){
   }
   return str + ' covfefe'
 }
+
+
+// In this Kata, you will be given a string that has lowercase letters and numbers.
+//  Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+
+function solve(s) {
+let currentNum = ''
+let max = ''
+for(let char of s){ 
+  if(char >= '0' && char <= '9'){
+    currentNum += char
+  }else{
+    max = Math.max(max, +currentNum || 0)
+    currentNum = ''
+  }
+}
+  return Math.max(max, +currentNum || 0)
+};
