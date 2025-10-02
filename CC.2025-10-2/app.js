3d37a7ac47c138e162ebf3ae834bea3e882c85ab
Absar Ahmad <absarahmad137@gmail.com>
2025-10-02 22:21:29 +0530

    codewars 7kys

--- CC.2025-10-2/app.js
+++ CC.2025-10-2/app.js
@@ -89,3 +89,12 @@ function arithmetic(a, b, operator){
  }
     return obj[operator]
 }
+
+// Your task is to write function factorial.
+function factorial(n){
+ let res = 1
+ for(let i = 1 ; i <= n; i ++){
+   res *= i
+ }
+  return res
+}
