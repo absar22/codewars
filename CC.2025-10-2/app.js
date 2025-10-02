4b6a1c1ad79697634c38b439b88c6c86c5630a77
Absar Ahmad <absarahmad137@gmail.com>
2025-10-02 11:12:44 +0530

    codewars 7kys

--- CC.2025-10-2/app.js
+++ CC.2025-10-2/app.js
@@ -33,3 +33,25 @@ function spoonerize(words) {
   let swap2 =  val1[0]
   return swap1 + val1.slice(1) + ' ' + swap2 + val2.slice(1)
 }
+
+
+// One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word.
+
+// Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+
+// instead of including i or I put the number 1 in the password;
+// instead of including o or O put the number 0 in the password;
+// instead of including s or S put the number 5 in the password.
+// Examples:
+// "Give me liberty or give me death"  --> "Gml0gmd"
+// "Keep Calm and Carry On"            --> "KCaC0"
+
+function makePassword(phrase) {
+   let password = phrase.split(' ').map(word => word[0]).join('')
+   return password.replaceAll("i", "1")
+    .replaceAll("I", "1")
+    .replaceAll("o", "0")
+    .replaceAll("O", "0")
+    .replaceAll("s", "5")
+    .replaceAll("S", "5")
+}
