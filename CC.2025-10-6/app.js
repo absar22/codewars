f3e1dd641ee1a7f8dc45c760c91a4f508aed8655
Absar Ahmad <absarahmad137@gmail.com>
2025-10-06 10:28:55 +0530

    codewars 7kys

--- CC.2025-10-6/app.js
+++ CC.2025-10-6/app.js
@@ -16,3 +16,24 @@ function spongeMeme(sentence) {
   }
   return upperLowerCaseSentence
 }
+
+
+// Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+
+// You task is to initialize the middle names (if there is any).
+
+// Examples
+// 'Jack Ryan'                   => 'Jack Ryan'
+// 'Lois Mary Lane'              => 'Lois M. Lane'
+// 'Dimitri'                     => 'Dimitri'
+// 'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+
+function initializeNames(name){
+ let names = name.trim().split(' ')
+let firstName = names[0]
+let lastName = names[names.length -1]
+if(firstName === lastName) return firstName
+if(names.length <= 2) return firstName + ' ' + lastName
+  let middleName = names.slice(1,-1).map(n => n[0] + '.').join(' ')
+  return firstName + ' ' + middleName + ' ' + lastName
+}
