1f23bfc7db4560fad5c09d5dffb96dbd6b5c2807
Absar Ahmad <absarahmad137@gmail.com>
2025-10-07 09:47:29 +0530

    codewars 7kys

---
+++ CC.2025-10-7/app.js
@@ -0,0 +1,19 @@
+// Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+// Note that if the range is given in capital letters, return the string in capitals also!
+
+// Examples
+// "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+// "h-o" ➞ "hijklmno"
+// "Q-Z" ➞ "QRSTUVWXYZ"
+// "J-J" ➞ "J"
+
+function gimmeTheLetters(sp) {
+  let results = ''
+  let val = sp.split('-')
+  let start = val[0].charCodeAt(0)
+  let end = val[1].charCodeAt(0)
+  for(let i = start; i <= end; i++){
+    results += String.fromCharCode(i)
+  }
+  return results
+}
