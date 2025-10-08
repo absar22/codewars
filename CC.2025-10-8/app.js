89992ea73ef3b45c065f014010e06a05d9c0ce20
Absar Ahmad <absarahmad137@gmail.com>
2025-10-08 10:36:20 +0530

    codewars 7kys

--- CC.2025-10-8/app.js
+++ CC.2025-10-8/app.js
@@ -15,3 +15,21 @@ function moveTen(s){
     }
   return results
 }
+
+// Write a function that removes every lone 9 that is inbetween 7s.
+
+// "79712312" --> "7712312"
+// "79797"    --> "777"
+
+
+function sevenAte9(str) {
+  let results = ''
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === '9' && str[i-1] === '7' && str[i + 1] === '7'){
+      continue
+    }else{
+      results += str[i]
+    }
+  }
+  return results
+}
