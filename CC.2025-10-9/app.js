b434816aa04a35c8df0fe73e75ad050e0f6e31b9
Absar Ahmad <absarahmad137@gmail.com>
2025-10-09 20:43:12 +0530

    codewars 6kys

--- CC.2025-10-9/app.js
+++ CC.2025-10-9/app.js
@@ -64,3 +64,27 @@ function countLanguages(list) {
  }
   return count
 }
+
+
+// Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+//   { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]
+
+
+function findSenior(list) {
+   let maxAge = list.map(obj => obj.age).sort((a,b) => b - a)[0]
+  return list.filter(obj => obj.age === maxAge)
+}
