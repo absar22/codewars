fe63be774e7e79d31c1f23ac3b5d66c95d8999c6
Absar Ahmad <absarahmad137@gmail.com>
2025-10-09 21:46:37 +0530

    codewars 6kys

--- CC.2025-10-9/app.js
+++ CC.2025-10-9/app.js
@@ -88,3 +88,25 @@ function findSenior(list) {
    let maxAge = list.map(obj => obj.age).sort((a,b) => b - a)[0]
   return list.filter(obj => obj.age === maxAge)
 }
+
+
+// Your task is to return:
+
+// true if all of the following continents / geographic zones will be represented by at least one developer: 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+//   { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+//   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+//   { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+//   { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+// ];
+// your function should return true as there is at least one developer from the required 5 geographic zones.
+
+function allContinents(list) {
+ let continents = ['Africa','Americas','Asia','Europe','Oceania']
+ let devContinents = list.map(obj => obj.continent)
+ return continents.every(arr => devContinents.includes(arr))
+}
