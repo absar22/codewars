a43d2abdc45898bfdd4e3fd92e1c389ff7155597
Absar Ahmad <absarahmad137@gmail.com>
2025-10-17 23:05:10 +0530

    codewars 7kyu

--- CC.2025-10-17/app.js
+++ CC.2025-10-17/app.js
@@ -66,3 +66,22 @@ function dotCalculator(equation) {
     else if(symbol === '//') results = leftSide / rightSide
   return '.'.repeat(results)
 }
+
+
+// You will be given a sequence of objects representing data about developers who have signed up to attend the
+//  next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+// ];
+// write a function that returns the average age of developers (rounded to the nearest integer). 
+// In the example above your function should return 50 (number).
+function getAverageAge(list) {
+  let ages =  list.map(obj => obj.age)
+  let sum =  ages.reduce((a,b) => a + b, 0)
+  let averageAge = sum / ages.length
+  return Math.round(averageAge)
+} 
