e14149be57ef3a9fc62a31388d23ac5a27bbaed7
Absar Ahmad <absarahmad137@gmail.com>
2025-10-18 18:43:49 +0530

    codewars 7kyu

--- CC.2025-10-18/app.js
+++ CC.2025-10-18/app.js
@@ -10,3 +10,16 @@ function lottery(str){
    let results = nums.filter((item, index) => nums.indexOf(item) === index).join('')
   return results ? results : 'One more run!'
 }
+
+// In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, or if the letter occurs only once in the string, -1 should be returned.
+
+// Examples:
+
+// for inputs  "Hello world!!!", 'l'  ->  return 3
+// for inputs  "Hello world!!!", 'A'  ->  return -1
+// Good luck ;) And don't forget to rate this kata if you liked it.
+
+function secondSymbol(s, symbol) {
+ let firstIndex = s.indexOf(symbol)
+ return s.indexOf(symbol, firstIndex + 1)
+}
