3d6e3317f0cc7500e68eb3e76ea4d2871acbab5d
Absar Ahmad <absarahmad137@gmail.com>
2025-10-18 14:51:24 +0530

    codewars 7kyu

---
+++ CC.2025-10-18/app.js
@@ -0,0 +1,12 @@
+// Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
+
+// Examples
+// "hPrBKWDH8yc6Lt5NQZWQ"  -->  "865"
+// "ynMAisVpHEqpqHBqTrwH"  -->  "One more run!"
+// "555"                   -->  "5"
+
+function lottery(str){
+   let nums =  str.split('').filter(letter => !isNaN(letter))
+   let results = nums.filter((item, index) => nums.indexOf(item) === index).join('')
+  return results ? results : 'One more run!'
+}
