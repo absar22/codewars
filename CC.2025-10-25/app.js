6cefd37c52bc1596c0190f2546a4e0b53bdb8189
Absar Ahmad <absarahmad137@gmail.com>
2025-10-25 11:09:03 +0530

    7kys

--- CC.2025-10-25/app.js
+++ CC.2025-10-25/app.js
@@ -42,3 +42,20 @@ function getStrings(city){
   
   return results 
 }
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// "Sky scra per" -> "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+
+
+function ghostBusters(building) {
+return building.includes(' ') ? building.replaceAll(' ','') : "You just wanted my autograph didn't you?"
+}
