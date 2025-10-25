4f5ae126ca5e4e884ae6e93b3ecbc39ab22a5de4
Absar Ahmad <absarahmad137@gmail.com>
2025-10-25 13:50:22 +0530

    7kys codewars

--- CC.2025-10-25/app.js
+++ CC.2025-10-25/app.js
@@ -107,3 +107,14 @@ function scrollingText(text){
   }
   return results
 }
+
+// Your task is very simple.
+//  Just write a function that takes an input string of
+//  lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+
+// Examples (input -> output)
+// "kata" -> false ('a' comes after 'k')
+// "ant" -> true (all characters are in alphabetical order)
+function alphabetic(s) {
+ return s.split('').sort().join('') === s
+}
